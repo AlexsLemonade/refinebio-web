1a2e06ed5aa335222f9c9c7ac864ccfcb1f993ab
nozomione <nozomione.online@gmail.com>
2022-09-12 20:16:26 -0400

    add config files for Sentry Next.js SDK

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// Sets a custom webpack configuration to use Next.js app with Sentry:
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const moduleExports = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  env: {
+    SENTRY_DSN: process.env.SENTRY_DSN
+  },
+  sentry: {
+    hideSourceMaps: true
+  }
 }
 
-module.exports = nextConfig
+const sentryWebpackPluginOptions = {
+  silent: true // Suppresses all logs
+}
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
---
+++ sentry.client.config.js
@@ -0,0 +1,10 @@
+// This file configures the initialization of Sentry on the browser:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+
+Sentry.init({
+  dsn: SENTRY_DSN
+})
