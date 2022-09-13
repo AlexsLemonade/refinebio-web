c46bc8d6b4dc70c69bd100d73017aa3a9a4d7941
nozomione <nozomione.online@gmail.com>
2022-09-13 17:12:30 -0400

    Updates includes: - add ErrorPage component in _error - add Snetry ErrorBounrday in _app

---
+++ src/pages/_error.js
@@ -0,0 +1,29 @@
+import { useRouter, useEffect } from 'next/router'
+
+const ErrorPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const forceRefresh = (url) => {
+      window.location = url
+    }
+
+    router.events.on('routeChangeStart', forceRefresh)
+  })
+
+  const goBack = () => {
+    router.back()
+  }
+
+  return (
+    <div>
+      <h1>We encountered an unexpected error.</h1>
+      <p>Please try again later..</p>
+      <button type="button" onClick={goBack}>
+        Go Back
+      </button>
+    </div>
+  )
+}
+
+export default ErrorPage
