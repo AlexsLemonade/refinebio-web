7812d2402de0e9a4322ba14b10abe6e023fbdf1e
nozomione <nozomione.online@gmail.com>
2022-09-14 09:49:28 -0400

    (temp) add a button that throws an error for Sentry integration test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
 const Home = () => (
   <div style={{ margin: ' 100px, auto' }}>
     <h1>Refinebio-web</h1>
+    <button
+      type="button"
+      onClick={() => {
+        throw new Error('Error')
+      }}
+    >
+      Generate Error
+    </button>
   </div>
 )
 
