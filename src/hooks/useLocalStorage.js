ffdf6b693c6a9f1b03ae4ac5cbf47a75a8fd5497
nozomione <nozomione.online@gmail.com>
2022-09-15 16:37:10 -0400

    Updates include: - add the edge-case to handle "window is underfiend" before the try...catch statements to prevent a build error - include help text in console.error to provide more context - (other minor updates)

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,22 +4,31 @@ export const useLocalStorage = (key, initialValue) => {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
+    // Prevent build error when "window is undefined"
+    if (typeof window === 'undefined') return initialValue
+
     try {
       // Get from local storage by key
-      if (typeof window === 'undefined') return initialValue
-
       const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
+      // Parse stored json or if null return initialValue
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
       // If error also return initialValue
-      console.error(error)
+      console.error(`Error reading localStorage key "${key}":`, error)
       return initialValue
     }
   })
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
+  // Return a wrapped version of useState's setter function that
+  // persists the new value to localStorage.
   const setValue = (value) => {
+    // Prevent build error when "window is undefined"
+    if (typeof window === 'undefined') {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Tried setting localStorage key “${key}” even though environment is not a client`
+      )
+    }
+
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
@@ -27,16 +36,14 @@ export const useLocalStorage = (key, initialValue) => {
       // Save state
       setStoredValue(valueToStore)
       // Save to local storage
-      if (typeof window !== 'undefined') {
-        if (valueToStore === undefined) {
-          window.localStorage.removeItem(key)
-        } else {
-          window.localStorage.setItem(key, JSON.stringify(valueToStore))
-        }
+      if (valueToStore === undefined) {
+        window.localStorage.removeItem(key)
+      } else {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore))
       }
     } catch (error) {
       // A more advanced implementation would handle the error case
-      console.error(error)
+      console.error(`Error setting localStorage key “${key}”:`, error)
     }
   }
   return [storedValue, setValue]
