5d9a97afaf0235681aee011b627cccd6bb98b41f
nozomione <nozomione.online@gmail.com>
2022-09-15 17:35:58 -0400

    add Grommet in top-level node _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,17 @@
 import * as Sentry from '@sentry/nextjs'
+import { Grommet } from 'grommet'
 import ErrorPage from 'pages/_error'
 
 const Fallback = () => <ErrorPage />
 
 const Portal = ({ Component, pageProps }) => {
   return (
-    <Sentry.ErrorBoundary fallback={Fallback} showDialog>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </Sentry.ErrorBoundary>
+    <Grommet plain>
+      <Sentry.ErrorBoundary fallback={Fallback} showDialog>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </Sentry.ErrorBoundary>
+    </Grommet>
   )
 }
 
