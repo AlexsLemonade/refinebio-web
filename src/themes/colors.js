eb16ef1b80b26fd773d0542a8ae4caad03167603
nozomione <nozomione.online@gmail.com>
2022-09-19 14:32:17 -0400

    re-define all the colors in the palette to be top-level, renamed variable names to use % instead of degree

--- src/themes/colors.js
+++ src/themes/colors.js
@@ -1,78 +1,78 @@
 export const colors = {
-  white: '#FDFDFD',
-  'alex-yellow': '#FFDD00',
   gradient: {
-    light: 'linear-gradient(180deg, #DFE9F3 7.13%, #FFFFFF 75%)',
-    dark: 'linear-gradient(180deg, rgba(12, 52, 131, 0.937981) 0%, #6B8CCE 131.35%, #A2B6DF 190.36%, #A1B6DF 190.36%, #A2B6DF 190.36%)'
+    dark: 'linear-gradient(180deg, rgba(12, 52, 131, 0.937981) 0%, #6B8CCE 131.35%, #A2B6DF 190.36%, #A1B6DF 190.36%, #A2B6DF 190.36%)',
+    light: 'linear-gradient(180deg, #DFE9F3 7.13%, #FFFFFF 75%)'
   },
-  // The integer refers to the degree of darkness/lightness of a color
-  // The greater the number, the lighter the color is
-  'alex-yellow-old': {
-    'degree-6': '#FFFDE4',
-    'degree-5': '#FFF55E',
-    base: '#F3E500',
-    'degree-3': '#DACE00',
-    'degree-2': '#C2B700',
-    'degree-1': '#AAA000'
-  },
-  'alex-navy': {
-    'degree-6': '#D6E8FF',
-    'degree-5': '#AED1FE',
-    'degree-4': '#5EA4FE',
-    'degree-3': '#0D76FF',
-    'degree-2': '#0051BC',
-    base: '#002F6C'
-  },
-  coral: {
-    'degree-10': '#FCF0EF',
-    'degree-9': '#F9E2DF',
-    'degree-8': '#F4C5BF',
-    'degree-7': '#EFA79F',
-    'degree-6': '#EA8A7F',
-    base: '#E56E60',
-    'degree-4': '#DB3B28',
-    'degree-3': '#A72A1B',
-    'degree-2': '#6F1C12',
-    'degree-1': '#370E09'
-  },
-  gray: {
-    'degree-4': '#F2F2F2',
-    'degree-3': '#CCC',
-    'degree-2': '#999',
-    'degree-1': '#4A4A4A'
-  },
-  forest: {
-    'degree-8': '#E6FDDC',
-    'degree-7': '#CEFCBA',
-    'degree-6': '#9DF975',
-    'degree-5': '#48D508',
-    base: '#319106',
-    'degree-3': '#277404',
-    'degree-2': '#1D5703',
-    'degree-1': '#133A02'
-  },
-  green: {
-    'degree-9': '#EAF6F0',
-    'degree-8': '#D6EEE1',
-    'degree-7': '#AEDEC4',
-    'degree-6': '#86CEA6',
-    'degree-5': '#5DBE89',
-    base: '#41A36D',
-    'degree-3': '#348257',
-    'degree-2': '#266141',
-    'degree-1': '#1A412B'
-  },
-  'refinebio-blue': {
-    'degree-9': '#E9F0F8',
-    'degree-8': '#D4E1F1',
-    'degree-7': '#AAC4E4',
-    'degree-6': '#80A6D6',
-    'degree-5': '#5689C9',
-    base: '#386DB0',
-    'degree-3': '#2C578C',
-    'degree-2': '#214169',
-    'degree-1': '#162B46'
-  }
+  // RULE: all color palettes should be define as top-level variables
+  /* 
+  /* Primary
+  */
+  black: '#000',
+  // Alex's Navy (old)
+  brand: '#002F6C',
+  'brand-tint-90': '#D6E8FF',
+  'brand-tint-80': '#AED1FE',
+  'brand-tint-60': '#5EA4FE',
+  'brand-tint-40': '#0D76FF',
+  'brand-tint-20': '#0051BC',
+  white: '#FDFDFD',
+  /* 
+  /* Accent
+  */
+  // Alex's Yellow (new)
+  'alex-yellow': '#FFDD00',
+  // Accent - Alex's Yellow (old)
+  'alex-yellow-old': '#F3E500',
+  'alex-yellow-old-tint-90': '#FFFDE4',
+  'alex-yellow-old-tint-40': '#FFF55E',
+  'alex-yellow-old-shade-10': '#DACE00',
+  'alex-yellow-old-shade-20': '#C2B700',
+  'alex-yellow-old-shade-30': '#AAA000',
+  // Coral
+  coral: '#E56E60',
+  'coral-tint-90': '#FCF0EF',
+  'coral-tint-80': '#F9E2DF',
+  'coral-tint-60': '#F4C5BF',
+  'coral-tint-40': '#EFA79F',
+  'coral-tint-20': '#EA8A7F',
+  'coral-shade-20': '#DB3B28',
+  'coral-shade-40': '#A72A1B',
+  'coral-shade-60': '#6F1C12',
+  'coral-shade-80': '#370E09',
+  // Forest
+  forest: '#319106',
+  'forest-tint-90': '#E6FDDC',
+  'forest-tint-80': '#CEFCBA',
+  'forest-tint-60': '#9DF975',
+  'forest-tint-20': '#48D508',
+  'forest-shade-20': '#277404',
+  'forest-shade-40': '#1D5703',
+  'forest-shade-60': '#133A02',
+  // Gray
+  'gray-shade-5': '#F2F2F2',
+  'gray-shade-20': '#CCC',
+  'gray-shade-40': '#999',
+  'gray-shade-70': '#4A4A4A',
+  // Green
+  'pastel-green': '#41A36D',
+  'pastel-green-tint-90': '#EAF6F0',
+  'pastel-green-tint-80': '#D6EEE1',
+  'pastel-green-tint-60': '#AEDEC4',
+  'pastel-green-tint-40': '#86CEA6',
+  'pastel-green-tint-20': '#5DBE89',
+  'pastel-green-shade-20': '#348257',
+  'pastel-green-shade-40': '#266141',
+  'pastel-green-shade-160': '#1A412B',
+  // Refinebio Blue
+  'refinebio-blue': '#386DB0',
+  'refinebio-blue-tint-90': '#E9F0F8',
+  'refinebio-blue-tint-80': '#D4E1F1',
+  'refinebio-blue-tint-60': '#AAC4E4',
+  'refinebio-blue-tint-40': '#80A6D6',
+  'refinebio-blue-tint-20': '#5689C9',
+  'refinebio-blue-shade-20': '#2C578C',
+  'refinebio-blue-shade-40': '#214169',
+  'refinebio-blue-shade-60': '#162B46'
 }
 
 export default colors
