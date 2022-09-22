46cad954f0c8781d2fa709af9298878fb603d630
nozomione <nozomione.online@gmail.com>
2022-09-21 21:20:05 -0400

    create IconBadge component and add necessary images to the ./src/images

---
+++ src/components/IconBadge.js
@@ -0,0 +1,33 @@
+import { Box, Paragraph } from 'grommet'
+import Accession from '../images/accession.svg'
+import Help from '../images/help.svg'
+import Organism from '../images/organism.svg'
+import MicroArray from '../images/microarray.svg'
+import MixedPlatform from '../images/mixed-platform.svg'
+import Rna from '../images/rna.svg'
+import Samples from '../images/samples.svg'
+
+const SVGs = {
+  Accession,
+  Help,
+  Organism,
+  MicroArray,
+  MixedPlatform,
+  Rna,
+  Samples
+}
+
+export const IconBadge = ({ name, label, ...props }) => {
+  const Badges = SVGs[name]
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Box direction="row" align="center" width="max-content" {...props}>
+      <Box>
+        <Badges role="presentation" aria-hidden="true" focusable="false" />
+      </Box>
+      <Paragraph margin={{ left: 'small' }}>{label}</Paragraph>
+    </Box>
+  )
+}
+
+export default IconBadge
