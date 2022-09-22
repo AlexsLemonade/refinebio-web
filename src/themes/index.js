d9f67fb24b73ef33e47d6695f1ae7bc1241cccaf
nozomione <nozomione.online@gmail.com>
2022-09-21 23:39:51 -0400

    import themes to '/themes/index.js''

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,10 +1,26 @@
+import button from 'themes/button'
+import checkBox from 'themes/checkBox'
 import global from 'themes/global'
 import heading from 'themes/heading'
 import paragraph from 'themes/paragraph'
+import radioButton from 'themes/radioButton'
+import select from 'themes/select'
+import tab from 'themes/tab'
+import tabs from 'themes/tabs'
+import text from 'themes/text'
+import { textInput } from 'themes/textInput'
 
 export const theme = {
+  button,
+  checkBox,
   global,
   heading,
-  paragraph
+  paragraph,
+  radioButton,
+  select,
+  tab,
+  tabs,
+  text,
+  textInput
 }
 export default theme
