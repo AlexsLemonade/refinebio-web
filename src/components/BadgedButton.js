7d87711f33a50b3f842d0780a90ae6914a12b184
nozomione <nozomione.online@gmail.com>
2022-09-21 21:18:46 -0400

    (edit) separate BadgedButton and NumberBadge(now import NumberBadge component instead of defining within)

--- src/components/BadgedButton.js
+++ src/components/BadgedButton.js
@@ -1,35 +1,10 @@
 import { Box, Button as GrommetButton } from 'grommet'
-import styled, { css } from 'styled-components'
-
-export const Badge = styled(Box)`
-  ${({ theme, count, light }) => css`
-    background: ${light
-      ? theme.global.colors.white
-      : theme.global.colors.brand};
-    border-radius: 30px;
-    color: ${light ? theme.global.colors.brand : theme.global.colors.white};
-    display: flex;
-    align-item: center;
-    font-size: 12px;
-    line-height: 16px;
-    padding: ${Number.isNaN(Number(count)) ? '0 4px' : 0};
-    position: absolute;
-    top: -8px;
-    right: -4px;
-    height: 16px;
-    min-width: 16px;
-    text-align: center;
-    vertical-align: middle;
-    z-index: 1;
-  `}
-`
+import { NumberBadge } from 'components/NumberBadge'
 
 export const BadgedButton = ({ count = 0, light = false, ...props }) => {
   return (
     <Box style={{ position: 'relative' }}>
-      <Badge light={light} count={count}>
-        {count}
-      </Badge>
+      <NumberBadge light={light} count={count} />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <GrommetButton {...props} />
     </Box>
