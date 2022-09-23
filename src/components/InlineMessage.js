23b7d19670e9a4f403dbfc37495da5db7c85fbf8
nozomione <nozomione.online@gmail.com>
2022-09-23 13:51:16 -0400

    (edit) set min height to be 24px in InlineMessage component

--- src/components/InlineMessage.js
+++ src/components/InlineMessage.js
@@ -28,8 +28,14 @@ export const InlineMessage = ({
     SVGs[`${status.substring(0, 1).toUpperCase()}${status.substring(1)}Icon`]
 
   return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <Box direction="row" align="center" width="max-content" {...props}>
+    <Box
+      direction="row"
+      align="center"
+      height={{ min: '24px' }}
+      width="max-content"
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
       {!labelOnly && (
         <Box margin={{ right: '8px' }}>
           <SVGIcon role="presentation" aria-hidden="true" focusable="false" />
