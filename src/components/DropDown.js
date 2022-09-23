1de36a176f6bea5ad37e8657cc2ecc4002591325
nozomione <nozomione.online@gmail.com>
2022-09-23 14:07:30 -0400

    (edit) add border on hover and active in DropDown

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,18 +1,25 @@
 import { Box, Menu as GrommetMenu } from 'grommet'
 import styled, { css } from 'styled-components'
 
+// NOTE: Add class name 'light' for the dark background
+
 const Wrapper = styled(Box)`
   font-family: 'Rubik', sans-serif;
   ${({ theme }) => css`
     button:first-child {
+      border-radius: 0;
       color: ${theme.global.colors.black};
-      padding: 4px 8px;
+      padding: 2px 0;
       svg {
         fill: ${theme.global.colors.black};
         stroke: ${theme.global.colors.black};
       }
+      &:hover {
+        border-bottom: 1px solid ${theme.global.colors.brand};
+      }
       &:hover,
       &[aria-expanded='true'] {
+        border-bottom: 1px solid ${theme.global.colors.brand};
         color: ${theme.global.colors.brand};
         svg {
           fill: ${theme.global.colors.brand};
