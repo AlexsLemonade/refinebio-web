125c0faf58f32f8156ee8ff5b465729568da870a
nozomione <nozomione.online@gmail.com>
2022-09-27 19:15:30 -0400

    Updates include: (edit) main.js - resolve path './../src' for import - register '../public' folder to 'staticDirs' - move the folder 'stories' to './storybook' and update its path (other) - create stories/components/Test.js(for testing) - (minor) delete exmaple files generated by Stroybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,29 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    './stories/**/*.stories.mdx',
+    './stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  staticDirs: ['../public'],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async (config, { configType }) => {
+    // (resource) https://storybook.js.org/docs/react/builders/webpack
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    config.resolve.modules.push(path.resolve(__dirname, './../src'))
+
+    // Return the altered config
+    return config
   }
 }
