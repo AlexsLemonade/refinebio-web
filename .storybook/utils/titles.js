1dc8ed6a44b5757e44ec7748039e772afcac1669
nozomione <nozomione.online@gmail.com>
2022-09-28 15:47:26 -0400

    add .storybook/utils/tite.js for managing each story's title by its category

---
+++ .storybook/utils/titles.js
@@ -0,0 +1,8 @@
+export default {
+  Badges: 'Badges/',
+  Buttons: 'Buttons/',
+  FormElements: 'Form Elements/',
+  FontStyles: 'Font Styles/',
+  InlineMessages: 'Inline Messages/',
+  Navigation: 'Navigation/'
+}
