8e413fe0c465572b20c2b87346f1f1e595feee93
nozomione <nozomione.online@gmail.com>
2022-10-04 16:02:25 -0400

    create the BackToTopButton component

---
+++ src/components/shared/BackToTopButton.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import { Button as sharedButton } from 'components/shared/Button'
+import styled from 'styled-components'
+
+const Button = styled(sharedButton)`
+  position: fixed;
+  bottom: 5vh;
+  right: 5vh;
+  z-index: 10;
+`
+
+export const BackToTopButton = () => {
+  const isWindow = typeof window !== 'undefined'
+  const OFFSET = 350
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const toggleShow = () => {
+      const scrolled = document.documentElement.scrollTop
+      if (scrolled > OFFSET) {
+        setShow(true)
+      } else if (scrolled <= OFFSET) {
+        setShow(false)
+      }
+    }
+
+    if (isWindow) {
+      window.addEventListener('scroll', toggleShow)
+    }
+
+    return () => {
+      if (isWindow) {
+        window.removeEventListener('scroll', toggleShow)
+      }
+    }
+  }, [])
+
+  return (
+    <Button
+      label="▲ Top"
+      onClick={() =>
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        })
+      }
+      style={{
+        animation: show ? 'fadeIn .35s' : 'none',
+        display: show ? 'block' : 'none'
+      }}
+    />
+  )
+}
+
+export default BackToTopButton
