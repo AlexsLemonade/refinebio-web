35ceac7d6b6fe2974a744404a26062e5d9c2a27e
nozomione <nozomione.online@gmail.com>
2022-10-04 09:17:14 -0400

    add the theme for FormField

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,5 +1,5 @@
 import { Box } from 'grommet'
-import { SearchCard } from 'components/SearchCard'
+// import { SearchCard } from 'components/SearchCard'
 import { SearchInput } from 'components/SearchInput'
 import { SearchFilters } from 'components/SearchFilters'
 
@@ -8,7 +8,7 @@ export const Search = () => {
     // TEMPORARY <Box> Wrapper
     <Box pad="medium">
       <SearchInput placeholder="Enter keyword" large />
-      <SearchCard />
+      {/* <SearchCard /> */}
       <SearchFilters />
     </Box>
   )
