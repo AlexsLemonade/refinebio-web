7155212cdbd2bd9a30e27c28a5ae0bd5d96ab130
nozomione <nozomione.online@gmail.com>
2022-10-04 01:22:26 -0400

    (temp) add /pages/search/index.js for rendering search page components in the UI

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,12 +1,15 @@
 import { Box } from 'grommet'
-import { SearchResult } from 'components/SearchResult'
+import { SearchCard } from 'components/SearchCard'
+import { SearchInput } from 'components/SearchInput'
+import { SearchFilters } from 'components/SearchFilters'
 
 export const Search = () => {
   return (
     // TEMPORARY <Box> Wrapper
     <Box pad="medium">
-      <SearchResult />
-      <SearchResult />
+      <SearchInput placeholder="Enter keyword" large />
+      <SearchCard />
+      <SearchFilters />
     </Box>
   )
 }
