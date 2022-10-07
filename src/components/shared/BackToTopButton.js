4bc03143bf21e0c3f2fc39e3bba43aeb47777b50
nozomione <nozomione.online@gmail.com>
2022-10-07 17:48:14 -0400

    (edit) update the BackToButton style to match the current refine.bio

--- src/components/shared/BackToTopButton.js
+++ src/components/shared/BackToTopButton.js
@@ -1,17 +1,54 @@
 import { useEffect, useState } from 'react'
+import { Box, Text } from 'grommet'
 import { Button as sharedButton } from 'components/shared/Button'
 import styled, { css } from 'styled-components'
 
 const Button = styled(sharedButton)`
-  padding: 4px 10px;
+  padding: 16px 8px 2px;
   position: fixed;
   bottom: 5vh;
   right: 5vh;
-  z-index: 10;
+  z-index: 1000;
+
+  > span {  
+    font-size: 12.8px;
+    font-weight: bolder;
+  }
    ${({ theme }) => css`
-   &:hover {
-    background: ${theme.global.colors['gray-shade-5']}
+     background: ${theme.global.colors['gray-shade-5']};
+     box-shadow: ${theme.global.elevation.light.medium};
+     > span {
+       color: ${theme.global.colors.brand};
+     }
+     &:hover {
+       box-shadow: none;
+     }
    `}
+  }
+`
+
+const ArrowIcon = styled(Box)`
+  ${({ theme }) => css`
+    border: 1px solid ${theme.global.colors.brand};
+    &::before {
+      border: 1px solid ${theme.global.colors.brand};
+    }
+  `}
+  border-width: 1px 1px 0 0;
+  margin: 0 calc(50% - 4px) 8px;
+  height: 6px;
+  width: 6px;
+  position: relative;
+  transform: rotate(-45deg);
+
+  &::before {
+    content: '';
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    left: -8px;
+    top: -11px;
   }
 `
 
@@ -43,7 +80,6 @@ export const BackToTopButton = () => {
 
   return (
     <Button
-      label="▲ Top"
       onClick={() =>
         window.scrollTo({
           top: 0,
@@ -54,7 +90,10 @@ export const BackToTopButton = () => {
         animation: show ? 'fadeIn .35s' : 'none',
         display: show ? 'block' : 'none'
       }}
-    />
+    >
+      <ArrowIcon />
+      <Text>Back to Top</Text>
+    </Button>
   )
 }
 
