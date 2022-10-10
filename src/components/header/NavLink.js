dcdc3a5a163c7a33b815f5243f475a4cba4ec3f1
nozomione <nozomione.online@gmail.com>
2022-10-10 15:26:27 -0400

    (edit) remove CSS class and use a prop to change the style for light color

--- src/components/header/NavLink.js
+++ src/components/header/NavLink.js
@@ -4,27 +4,20 @@ import styled, { css } from 'styled-components'
 // NOTE: Set the prop 'light' to true for the dark background
 
 const Wrapper = styled(Box)`
-  ${({ theme }) => css`
+  ${({ theme, light }) => css`
     font-family: 'Rubik', sans-serif;
     padding: 2px 0;
     width: max-content;
     a {
-      color: ${theme.global.colors.black};
+      color: ${light ? theme.global.colors.white : theme.global.colors.black};
       &:hover {
-        color: ${theme.global.colors.brand};
+        color: ${light ? theme.global.colors.white : theme.global.colors.brand};
         text-decoration: none;
       }
     }
     &:hover {
-      border-bottom: 1px solid ${theme.global.colors.brand};
-    }
-    &.light {
-      a {
-        color: ${theme.global.colors.white};
-      }
-      &:hover {
-        border-bottom: 1px solid ${theme.global.colors.white};
-      }
+      border-bottom: 1px solid
+        ${light ? theme.global.colors.white : theme.global.colors.brand};
     }
   `}
 `
@@ -32,7 +25,7 @@ const Wrapper = styled(Box)`
 export const NavLink = ({ light = false, link, children, ...props }) => {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <Wrapper className={light ? 'light' : ''} {...props}>
+    <Wrapper light={light} {...props}>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <GrommetAnchor link={link} {...props}>
         {children}
