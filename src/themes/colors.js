6f379a85ce5f5291ee34e7d2ef0581bb79152a9e
nozomione <nozomione.online@gmail.com>
2022-10-29 18:57:55 -0400

    (themes) define formField theme and update colors/paragraph theme values

--- src/themes/colors.js
+++ src/themes/colors.js
@@ -84,6 +84,9 @@ export const colors = {
   gradient:
     'linear-gradient(180deg, rgba(12, 52, 131, 0.937981) 0%, #6B8CCE 131.35%, #A2B6DF 190.36%, #A1B6DF 190.36%, #A2B6DF 190.36%)',
   'gradient-light': 'linear-gradient(180deg, #DFE9F3 7.13%, #FFFFFF 75%)',
+  gradient_light: 'linear-gradient(360deg, #DFE9F3 7.13%, #FFFFFF 75%)',
+  gradient_dark:
+    'linear-gradient(179.93deg, rgba(12, 52, 131, 0.937981) -11.89%, rgba(25, 64, 142, 0.946665) 21.67%, #6b8cce 75.91%, #a2b6df 129.12%, #a2b6df 192.17%)',
   // default font color
   text: {
     dark: '',
---
+++ src/themes/formField.js
@@ -0,0 +1,5 @@
+export default {
+  border: {
+    color: 'transparent'
+  }
+}
