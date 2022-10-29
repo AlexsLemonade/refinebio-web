b962e6c77ebd6adee35136db60b8ae38f05be9b0
nozomione <nozomione.online@gmail.com>
2022-10-29 18:36:38 -0400

    create FixedContainer(an inner wrapper) - has fixed width 1250px(based on mockup)

---
+++ src/components/shared/FixedContainer.js
@@ -0,0 +1,17 @@
+import { useResponsive } from 'hooks/useResponsive'
+import { Box } from 'grommet'
+
+export const FixedContainer = ({ children, ...props }) => {
+  const { setResponsive } = useResponsive()
+  return (
+    <Box
+      margin={{ horizontal: 'auto' }}
+      pad={setResponsive({ horizontal: 'large' }, { horizontal: 'medium' })}
+      width="1250px" // to preserve UI layout for wider screens
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      {children}
+    </Box>
+  )
+}
