91d0716ba842b2656dba254e7fb83d9ee7f7f20c
nozomione <nozomione.online@gmail.com>
2022-10-29 18:16:06 -0400

    create useResponsive hook and add basic breakpoints/edgeSize in themes/global

---
+++ src/hooks/useResponsive.js
@@ -0,0 +1,30 @@
+import { useContext, useEffect } from 'react'
+import { ResponsiveContext } from 'grommet'
+
+/*
+  small = a phone <= 500
+  medium = a tablet <= 1024 
+  large = a computer >= 1025
+*/
+
+export const useResponsive = () => {
+  const viewport = useContext(ResponsiveContext)
+
+  useEffect(() => {}, [viewport])
+
+  const setResponsive = (small, medium, large) => {
+    if (!large) {
+      return viewport === 'small' ? small : medium
+    }
+
+    // eslint-disable-next-line no-nested-ternary
+    return viewport === 'small' ? small : viewport === 'medium' ? medium : large
+  }
+
+  return {
+    viewport,
+    setResponsive
+  }
+}
+
+export default useResponsive
