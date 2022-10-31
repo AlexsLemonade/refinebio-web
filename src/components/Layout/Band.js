65ec74c302d2d2cf6920dbd0a1d6b46842f3450e
nozomione <nozomione.online@gmail.com>
2022-10-31 12:45:55 -0400

    create Band

---
+++ src/components/Layout/Band.js
@@ -0,0 +1,32 @@
+import { Box } from 'grommet'
+import { FixedContainer } from 'components/shared/FixedContainer'
+
+export const Band = ({ ...props }) => {
+  return (
+    <Box
+      background="gradient_dark"
+      elevation="large"
+      height="420px"
+      width="100%"
+      style={{ position: 'absolute', zIndex: -1 }}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      <FixedContainer height="100%">
+        <Box
+          aria-hidden
+          background={{
+            opacity: '0.1',
+            image: " url('circus-plot.svg')",
+            position: 'center 90%',
+            repeat: 'no-repeat'
+          }}
+          fill
+          height="100%"
+        />
+      </FixedContainer>
+    </Box>
+  )
+}
+
+export default Band
