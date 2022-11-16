b8fd484a073815017411d7bd8e2f0f88eaf06bf3
nozomione <nozomione.online@gmail.com>
2022-11-16 16:47:24 -0500

    (edit) remove Wrapper and styled-component from Logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,30 +1,23 @@
 import { Box } from 'grommet'
 import { Anchor } from 'components/shared/Anchor'
 import { SrOnly } from 'components/shared/SrOnly'
-import styled, { css } from 'styled-components'
 import { LogoSvg } from '../../images/logo.svg'
 
 // NOTE: Set the prop 'light' to true for the dark background
 
-const Wrapper = styled(Box)`
-  ${({ theme, light }) => css`
-  a {
-    line-height: 0;
-    color: ${
-      light ? theme.global.colors.white : theme.global.colors['alex-navy']
-    };
-  `}
-  } 
-`
-
 export const Logo = ({ light = false }) => {
   return (
-    <Wrapper light={light}>
-      <Anchor href="/" underline={false}>
+    <Box light={light}>
+      <Anchor
+        color={light ? 'white' : 'alex-navy'}
+        href="/"
+        underline={false}
+        style={{ lineHeight: 0 }}
+      >
         <LogoSvg role="img" title="refine.bio" />
         <SrOnly label="Go to refine.bio homepage " />
       </Anchor>
-    </Wrapper>
+    </Box>
   )
 }
 
