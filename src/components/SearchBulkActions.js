fd147815541552a0e5a724b525bce8fa71c8ed73
nozomione <nozomione.online@gmail.com>
2022-11-29 14:01:27 -0500

    (edit) optimize SearchBuldActions component and add a align prop in shared/Row component

--- src/components/SearchBulkActions.js
+++ src/components/SearchBulkActions.js
@@ -1,20 +1,8 @@
-import {
-  Box,
-  CheckBox,
-  FormField,
-  Select as GrommetSelect,
-  Text
-} from 'grommet'
+import { Box, CheckBox, Select as GrommetSelect, Text } from 'grommet'
 import { Button } from 'components/shared/Button'
+import { Row } from 'components/shared/Row'
 import styled from 'styled-components'
 import data from 'api/data'
-
-const Wrapper = styled(Box)`
-  * {
-    align-items: center;
-    flex-direction: row;
-  }
-`
 
 const PageSizeControl = styled(GrommetSelect)`
   width: 36px;
@@ -26,39 +14,33 @@ const SortByControl = styled(GrommetSelect)`
 
 export const SearchBulkActions = () => {
   return (
-    <Wrapper pad={{ bottom: 'medium' }}>
-      <Box justify="between" margin={{ bottom: 'small' }}>
-        <Box>
-          <FormField a11yTitle="Page size" htmlFor="page-size">
-            Showing
-            <PageSizeControl
-              defaultValue={data.SearchBulkActions.PAGE_SIZES[0]}
-              id="page-size"
-              options={data.SearchBulkActions.PAGE_SIZES}
-              margin={{ horizontal: 'xxsmall' }}
-            />
-          </FormField>
+    <Box pad={{ bottom: 'medium' }}>
+      <Row margin={{ bottom: 'small' }}>
+        <Row align="center">
+          Showing
+          <PageSizeControl
+            defaultValue={data.SearchBulkActions.PAGE_SIZES[0]}
+            options={data.SearchBulkActions.PAGE_SIZES}
+            margin={{ horizontal: 'xxsmall' }}
+          />
           <Text>of {data.SearchBulkActions.totalResults} results</Text>
-        </Box>
-        <Box>
-          <FormField a11yTitle="Sort by" htmlFor="sort-by">
-            Sort by
-            <SortByControl
-              defaultValue={data.SearchBulkActions.SORTBY_OPTIONS[0]}
-              id="sort-by"
-              options={data.SearchBulkActions.SORTBY_OPTIONS}
-              margin={{ horizontal: 'xxsmall' }}
-            />
-          </FormField>
-        </Box>
+        </Row>
+        <Row align="center">
+          Sort by
+          <SortByControl
+            defaultValue={data.SearchBulkActions.SORTBY_OPTIONS[0]}
+            options={data.SearchBulkActions.SORTBY_OPTIONS}
+            margin={{ horizontal: 'xxsmall' }}
+          />
+        </Row>
         <Box>
           <Button label="Add Page to Dataset" secondary />
         </Box>
-      </Box>
+      </Row>
       <Box>
         <CheckBox label="Hide non-downloadable experiments" />
       </Box>
-    </Wrapper>
+    </Box>
   )
 }
 
