1ec7842d8c62937a8db9aed7389c6be0bc96fe6c
nozomione <nozomione.online@gmail.com>
2022-11-29 15:53:42 -0500

    (edit) remove Wrappers and styled-component for PageSizeControl and SortByControl in SearchBulkActions

--- src/components/SearchBulkActions.js
+++ src/components/SearchBulkActions.js
@@ -1,16 +1,7 @@
-import { Box, CheckBox, Select as GrommetSelect, Text } from 'grommet'
+import { Box, CheckBox, Select, Text } from 'grommet'
 import { Button } from 'components/shared/Button'
 import { Row } from 'components/shared/Row'
-import styled from 'styled-components'
 import data from 'api/data'
-
-const PageSizeControl = styled(GrommetSelect)`
-  width: 36px;
-`
-
-const SortByControl = styled(GrommetSelect)`
-  width: 160px;
-`
 
 export const SearchBulkActions = () => {
   return (
@@ -18,20 +9,24 @@ export const SearchBulkActions = () => {
       <Row margin={{ bottom: 'small' }}>
         <Row align="center">
           Showing
-          <PageSizeControl
-            defaultValue={data.SearchBulkActions.PAGE_SIZES[0]}
-            options={data.SearchBulkActions.PAGE_SIZES}
-            margin={{ horizontal: 'xxsmall' }}
-          />
+          <Box width="88px">
+            <Select
+              defaultValue={data.SearchBulkActions.PAGE_SIZES[0]}
+              options={data.SearchBulkActions.PAGE_SIZES}
+              margin={{ horizontal: 'xxsmall' }}
+            />
+          </Box>
           <Text>of {data.SearchBulkActions.totalResults} results</Text>
         </Row>
         <Row align="center">
           Sort by
-          <SortByControl
-            defaultValue={data.SearchBulkActions.SORTBY_OPTIONS[0]}
-            options={data.SearchBulkActions.SORTBY_OPTIONS}
-            margin={{ horizontal: 'xxsmall' }}
-          />
+          <Box width="208px">
+            <Select
+              defaultValue={data.SearchBulkActions.SORTBY_OPTIONS[0]}
+              options={data.SearchBulkActions.SORTBY_OPTIONS}
+              margin={{ horizontal: 'xxsmall' }}
+            />
+          </Box>
         </Row>
         <Box>
           <Button label="Add Page to Dataset" secondary />
