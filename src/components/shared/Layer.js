9d9d2506c31ba3de882919de7bc5741e6c13237d
nozomione <nozomione.online@gmail.com>
2022-12-05 18:29:38 -0500

    (mionr) update Layer/SearchBox/Row components to match with the latest changes

--- src/components/shared/Layer.js
+++ src/components/shared/Layer.js
@@ -1,9 +1,17 @@
 import { Layer as GrommetLayer } from 'grommet'
 import { useResponsive } from 'hooks/useResponsive'
 
-export const Layer = ({ position = '', show = false, children }) => {
+export const Layer = ({
+  position = '',
+  tabletMode = false,
+  show = false,
+  children
+}) => {
   const { viewport } = useResponsive()
-  if (viewport !== 'small') return children
+  if (viewport === 'large' || (viewport === 'medium' && !tabletMode)) {
+    return children
+  }
+
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>{show && <GrommetLayer position={position}>{children}</GrommetLayer>}</>
--- src/components/shared/Row.js
+++ src/components/shared/Row.js
@@ -1,11 +1,18 @@
 import { useResponsive } from 'hooks/useResponsive'
 import { Box } from 'grommet'
 
-export const Row = ({ justify = 'between', children, ...props }) => {
+export const Row = ({
+  align = 'start',
+  direction = '',
+  justify = 'between',
+  children,
+  ...props
+}) => {
   const { setResponsive } = useResponsive()
   return (
     <Box
-      direction={setResponsive('column', 'row')}
+      align={align}
+      direction={direction || setResponsive('column', 'row')}
       justify={justify}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
