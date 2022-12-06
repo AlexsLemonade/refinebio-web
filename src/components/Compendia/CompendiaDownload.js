db9a8e2634721031123c8cf397315885d6c455b8
nozomione <nozomione.online@gmail.com>
2022-12-06 00:20:11 -0500

    create Compendia/CompendiaDownloadBlock and CompendiaDownload components

---
+++ src/components/Compendia/CompendiaDownload.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from 'react'
+import { useRefinebioContext } from 'hooks/useRefinebioContext'
+import { useResponsive } from 'hooks/useResponsive'
+import {
+  Anchor,
+  Box,
+  CheckBox,
+  Heading,
+  Select as GrommetSelect,
+  Text
+} from 'grommet'
+import { Icon } from 'components/shared/Icon'
+import { Button } from 'components/shared/Button'
+import { InlineMessage } from 'components/shared/InlineMessage'
+import { Row } from 'components/shared/Row'
+import styled from 'styled-components'
+
+const CustomSelect = styled(GrommetSelect)`
+  padding-left: 32px;
+`
+
+export const CompendiaDownload = ({ heading, isNormalized }) => {
+  const { token, setToken } = useRefinebioContext()
+  const { setResponsive } = useResponsive()
+  const [agree, setAgree] = useState(!!token)
+  const [options, setOptions] = useState([])
+  const [selectedOption, setSelectedOption] = useState('')
+
+  const clickHandle = () => {
+    // TEMP
+    setToken(true)
+  }
+
+  useEffect(() => {
+    setOptions(() =>
+      isNormalized ? ['One', 'Two', 'Three'] : ['Four', 'Five', 'Six']
+    )
+  }, [])
+
+  useEffect(() => {}, [selectedOption])
+
+  return (
+    <Box background="white" pad={setResponsive('medium', 'large', 'xlarge')}>
+      <Heading
+        level={2}
+        margin={{ bottom: 'medium' }}
+        size={setResponsive('h2_small', 'h2_large')}
+      >
+        Download the {heading}
+      </Heading>
+      <Box
+        as="label"
+        margin={{ bottom: 'medium' }}
+        style={{ font: "22px 'Rubik', sans-serif" }}
+      >
+        Choose Organism
+      </Box>
+      <Box style={{ position: 'relative' }}>
+        <Box
+          style={{ position: 'absolute', left: '6px', top: '8px', zIndex: 1 }}
+        >
+          <Icon name="Search" size="20px" />
+        </Box>
+        <CustomSelect
+          id="select-organism"
+          options={options}
+          value={selectedOption}
+          placeholder="Search for an organism"
+          onChange={({ option }) => setSelectedOption(option)}
+        />
+      </Box>
+      {!isNormalized && (
+        <Box margin={{ top: setResponsive('small', 'medium') }}>
+          <InlineMessage label="Data is not normalized or aggregated." />
+        </Box>
+      )}
+      <Box margin={{ vertical: setResponsive('small', 'medium') }}>
+        <CheckBox
+          label={
+            <Text>
+              I agree to the <Anchor href="#url">Terms of Use</Anchor>
+            </Text>
+          }
+          onClick={() => setAgree(!agree)}
+        />
+      </Box>
+      <Row>
+        <Box margin={{ bottom: setResponsive('small', 'small', 'none') }}>
+          {selectedOption && <Text>Download Size: 65.91 MB</Text>}
+        </Box>
+        <Box align={setResponsive('start', 'end')}>
+          <Button
+            label="Download Now"
+            disabled={!agree}
+            primary
+            responsive
+            onClick={clickHandle}
+          />
+        </Box>
+      </Row>
+    </Box>
+  )
+}
+
+export default CompendiaDownload
---
+++ src/components/Compendia/CompendiaDownloadBlock.js
@@ -0,0 +1,104 @@
+import { memo } from 'react'
+import { useResponsive } from 'hooks/useResponsive'
+import { Box, Heading, Paragraph, Text } from 'grommet'
+import { Column } from 'components/shared/Column'
+import { FixedContainer } from 'components/shared/FixedContainer'
+import { Row } from 'components/shared/Row'
+import { CompendiaDownload } from './CompendiaDownload'
+
+export const CompendiaDownloadBlock = ({ type = 'normalized' }) => {
+  const { setResponsive } = useResponsive()
+  const isNormalized = type === 'normalized'
+
+  const svgImage = isNormalized
+    ? 'normalizaed-curve.svg'
+    : 'gene-expression-matrix.svg'
+
+  const heading = isNormalized
+    ? 'Compendia Normalized'
+    : 'RNA-seq Sample Compendia'
+
+  const TextContent = (
+    <Paragraph color="white" size="xlarge">
+      {isNormalized ? (
+        'Normalized Compendia are the collection of all the samples available on refine.bio, aggregated and normalized by species.'
+      ) : (
+        <>
+          Get the collection of Salmon output as{' '}
+          <Text
+            color="coral-shade-20"
+            size="xlarge"
+            style={{ background: '#aac4e4' }}
+          >
+            quant.sf
+          </Text>{' '}
+          files for an organism's RNA-seq samples for maximum flexibility.
+        </>
+      )}
+    </Paragraph>
+  )
+
+  return (
+    <Box margin={{ top: 'basex12' }}>
+      <Box
+        background="gradient_blue_dark"
+        pad={{
+          top: 'basex8',
+          bottom: 'xlarge'
+        }}
+      >
+        <FixedContainer>
+          <Box
+            background={{
+              image: `url(${svgImage})`,
+              position: 'center',
+              repeat: 'no-repeat',
+              size: '100%'
+            }}
+            height="106px"
+            width="106px"
+            style={{
+              top: '-112px',
+              left: '50%',
+              position: 'absolute',
+              transform: 'translateX(-50%)'
+            }}
+          />
+          <Heading
+            level={2}
+            color="white"
+            margin={{ bottom: 'medium' }}
+            size={setResponsive('h2_small', 'h2_large')}
+            style={{ textShadow: '0 3px 19px rgba(0,0,0,.5)' }}
+            alignSelf="center"
+          >
+            {heading}
+          </Heading>
+          <Row direction={setResponsive('column', 'column', 'row')}>
+            <Column
+              flexValue={setResponsive('1 1 auto', '1 1 auto', '1 1 0')}
+              margin={{ right: setResponsive('none', 'none', 'xlarge') }}
+            >
+              {TextContent}
+            </Column>
+            <Column
+              flexValue={setResponsive('1 1 auto', '1 1 auto', '1 1 0')}
+              margin={{
+                top: setResponsive('medium', 'medium', 'none'),
+                left: setResponsive('none', 'none', 'xlarge')
+              }}
+              width="100%"
+            >
+              <CompendiaDownload
+                heading={heading}
+                isNormalized={isNormalized}
+              />
+            </Column>
+          </Row>
+        </FixedContainer>
+      </Box>
+    </Box>
+  )
+}
+
+export default memo(CompendiaDownloadBlock)
