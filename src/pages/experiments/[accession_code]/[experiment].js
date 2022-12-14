0188966c72f3cc689d5a835856e2a07d79c2f518
nozomione <nozomione.online@gmail.com>
2022-12-14 16:13:06 -0500

    (edit) add the 'Back to Results' button in experiments

--- src/pages/experiments/[accession_code]/[experiment].js
+++ src/pages/experiments/[accession_code]/[experiment].js
@@ -1,7 +1,25 @@
+import { useRouter } from 'next/router'
 import { Box } from 'grommet'
+import { Button } from 'components/shared/Button'
+import { FixedContainer } from 'components/shared/FixedContainer'
 
 export const Experiment = () => {
-  return <Box>Experiment</Box>
+  const router = useRouter()
+
+  return (
+    <Box>
+      <FixedContainer pad="large">
+        <Button
+          label="Back to Results"
+          secondary
+          responsive
+          onClick={() => {
+            router.back()
+          }}
+        />
+      </FixedContainer>
+    </Box>
+  )
 }
 
 export default Experiment
