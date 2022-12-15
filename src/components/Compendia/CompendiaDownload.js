86cdd89696f65617ebbd15f62ee1f4a5d3ae6db4
nozomione <nozomione.online@gmail.com>
2022-12-14 20:41:44 -0500

    (edit) implement the search field for the CompendiaDownload(previously using a form select) and add the fitering based on a user input

--- src/components/Compendia/CompendiaDownload.js
+++ src/components/Compendia/CompendiaDownload.js
@@ -1,46 +1,120 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, memo } from 'react'
 import { useRefinebioContext } from 'hooks/useRefinebioContext'
 import { useResponsive } from 'hooks/useResponsive'
-import {
-  Anchor,
-  Box,
-  CheckBox,
-  Heading,
-  Select as GrommetSelect,
-  Text
-} from 'grommet'
+import { Anchor, Box, CheckBox, Heading, Text } from 'grommet'
 import { Icon } from 'components/shared/Icon'
 import { Button } from 'components/shared/Button'
 import { Column } from 'components/shared/Column'
+import { List } from 'components/shared/List'
 import { InlineMessage } from 'components/shared/InlineMessage'
 import { Row } from 'components/shared/Row'
+import { SearchBox } from 'components/shared/SearchBox'
 import { formatBytes } from 'helpers/formatBytes'
 import { formatString } from 'helpers/formatString'
-import styled from 'styled-components'
 import { links } from 'config'
+import styled, { css } from 'styled-components'
 import data from 'api/mockDataCompendia'
 
-const CustomSelect = styled(GrommetSelect)`
-  padding-left: 32px;
+const Li = styled(Box)`
+  ${({ theme, selected }) => css`
+    width: 100%;
+    background: ${selected
+      ? theme.global.colors.brand
+      : theme.global.colors.white};
+    button {
+      color: ${selected
+        ? theme.global.colors.white
+        : theme.global.colors.black};
+    }
+    &:hover {
+      background: ${selected
+        ? theme.global.colors.brand
+        : theme.global.colors['gray-shade-5']};
+
+      button {
+        color: ${selected
+          ? theme.global.colors.white
+          : theme.global.colors.brand};
+      }
+    }
+  `}
 `
+
+const ListItem = ({ selectedOption, label, children }) => {
+  return (
+    <Li
+      as="li"
+      selected={selectedOption === label}
+      style={{ listStyle: 'none' }}
+    >
+      {children}
+    </Li>
+  )
+}
 
 export const CompendiaDownload = ({ heading, isNormalized }) => {
   const { token, setToken } = useRefinebioContext()
   const { setResponsive } = useResponsive()
   const [agree, setAgree] = useState(!!token)
+  const [userInput, setUserInput] = useState('')
   const [options, setOptions] = useState([])
-  const [selectedOption, setSelectedOption] = useState({
-    result: null,
-    name: ''
-  })
+  const [filteredOptions, setFilteredOptions] = useState([])
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [showOptions, setShowOptions] = useState(false)
 
-  const clickHandle = () => {
+  const updateFilteredOptions = (val) => {
+    if (val.trim() !== '') {
+      setFilteredOptions(() =>
+        options.filter((option) =>
+          formatString(option.primary_organism_name)
+            .toLowerCase()
+            .startsWith(val.toLowerCase())
+        )
+      )
+    } else {
+      setFilteredOptions(options)
+    }
+  }
+
+  const handleChange = (val) => {
+    if (val.trim() === '' || val !== userInput) {
+      setSelectedOption(null)
+    }
+
+    clearTimeout(timer)
+    const timer = setTimeout(() => {
+      setUserInput(val)
+    }, 200)
+
+    updateFilteredOptions(val)
+  }
+
+  const handleBlur = () => {
+    clearTimeout(timer)
+    const timer = setTimeout(() => {
+      setShowOptions(false)
+    }, 200)
+  }
+
+  const handleFocus = () => {
+    setShowOptions(true)
+  }
+
+  const handleClick = (option) => {
+    setSelectedOption(option)
+    setUserInput(formatString(option.primary_organism_name))
+    setShowOptions(false)
+    updateFilteredOptions(formatString(option.primary_organism_name))
+  }
+
+  const downloadCompendia = () => {
     // TEMP
     setToken(true)
   }
 
   useEffect(() => {
     setOptions(() => (isNormalized ? data[0].results : data[1].results))
+    setFilteredOptions(options)
   }, [options])
 
   return (
@@ -61,28 +135,67 @@ export const CompendiaDownload = ({ heading, isNormalized }) => {
       </Box>
       <Box style={{ position: 'relative' }}>
         <Box
-          style={{ position: 'absolute', left: '6px', top: '8px', zIndex: 1 }}
+          style={{
+            position: 'absolute',
+            right: '10px',
+            top: '10px',
+            zIndex: 1
+          }}
         >
-          <Icon name="Search" size="20px" />
+          <Icon name="ChevronDown" size="xsmall" />
         </Box>
-        <CustomSelect
-          id="select-organism"
-          label={options.map((option) =>
-            formatString(option.primary_organism_name)
+        <Box style={{ position: 'relative' }}>
+          <SearchBox
+            padding="16px 32px"
+            placeholder="Search for an organism"
+            size="small"
+            reverse={false}
+            responsive
+            value={userInput}
+            blurHandler={handleBlur}
+            changeHandler={(e) => handleChange(e.target.value)}
+            focusHandler={handleFocus}
+          />
+          {showOptions && (
+            <Box
+              background="white"
+              border={{ color: 'brand', size: 'medium' }}
+              margin={{ top: 'xlarge' }}
+              height={{ max: '200px' }}
+              width="100%"
+              style={{ overflowY: 'scroll', position: 'absolute', zIndex: 1 }}
+            >
+              <List flexDirection="column">
+                {filteredOptions.map((option) => (
+                  <ListItem
+                    key={option.primary_organism_name}
+                    selectedOption={
+                      selectedOption
+                        ? formatString(selectedOption.primary_organism_name)
+                        : null
+                    }
+                    label={formatString(option.primary_organism_name)}
+                  >
+                    <Button
+                      background="transparent"
+                      color="black"
+                      label={formatString(option.primary_organism_name)}
+                      width="100%"
+                      style={{
+                        borderRadius: '0',
+                        display: 'block',
+                        whiteSpace: 'nowrap',
+                        padding: '8px 16px',
+                        textAlign: 'left'
+                      }}
+                      clickHandler={() => handleClick(option)}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
           )}
-          options={options.map((option) =>
-            formatString(option.primary_organism_name)
-          )}
-          value={selectedOption.primary_organism_name}
-          placeholder="Search for an organism"
-          onChange={({ option }) =>
-            options.map(
-              (o) =>
-                option === formatString(o.primary_organism_name) &&
-                setSelectedOption({ result: o, name: option })
-            )
-          }
-        />
+        </Box>
       </Box>
       {!isNormalized && (
         <Box margin={{ top: setResponsive('small', 'medium') }}>
@@ -104,11 +217,11 @@ export const CompendiaDownload = ({ heading, isNormalized }) => {
           flexValue={setResponsive('1 1 auto', '1 1 auto', '1 1 0')}
           margin={{ bottom: setResponsive('small', 'small', 'none') }}
         >
-          {selectedOption.result && (
+          {selectedOption && (
             <Box animation={{ type: 'fadeIn', duration: 800 }}>
               <Text>
                 Download Size:{' '}
-                {formatBytes(selectedOption.result.computed_file.size_in_bytes)}
+                {formatBytes(selectedOption.computed_file.size_in_bytes)}
               </Text>
             </Box>
           )}
@@ -119,10 +232,10 @@ export const CompendiaDownload = ({ heading, isNormalized }) => {
         >
           <Button
             label="Download Now"
-            disabled={!agree || !selectedOption.result}
+            disabled={!agree || !selectedOption}
             primary
             responsive
-            onClick={clickHandle}
+            clickHandler={downloadCompendia}
           />
         </Column>
       </Row>
@@ -130,4 +243,4 @@ export const CompendiaDownload = ({ heading, isNormalized }) => {
   )
 }
 
-export default CompendiaDownload
+export default memo(CompendiaDownload)
