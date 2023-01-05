cfbf5054202d2c6257b9a02fdc1632f25ca8e668
nozomione <nozomione.online@gmail.com>
2023-01-05 16:45:06 -0500

    create PageSizes component and add to the data table

---
+++ src/components/shared/DataTable/PageSizes.js
@@ -0,0 +1,27 @@
+import { Box, Select, Text } from 'grommet'
+
+export const PageSizes = ({ count, pageSize, pageSizes, setPageSize }) => {
+  const handleChange = (value) => {
+    setPageSize(value)
+  }
+
+  return (
+    <Box align="center" direction="row">
+      <Text margin={{ right: 'xsmall' }}>Show</Text>
+      <Box width="84px">
+        <Select
+          id="page-sizes"
+          defaultValue={pageSize}
+          options={pageSizes}
+          value={pageSize}
+          onChange={(e) => handleChange(Number(e.target.value))}
+        />
+      </Box>
+      <Text margin={{ left: 'xsmall' }}>
+        of {count.toLocaleString()} Total Samples
+      </Text>
+    </Box>
+  )
+}
+
+export default PageSizes
