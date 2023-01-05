c684ccaba9502b1c40765040189d8db992076362
nozomione <nozomione.online@gmail.com>
2023-01-05 00:27:58 -0500

    create GlobalFilter component and add to DataTable

--- src/components/shared/DataTable/DataTable.js
+++ src/components/shared/DataTable/DataTable.js
@@ -1,51 +1,84 @@
 import { memo } from 'react'
-import { Table, TableHeader, TableRow, TableCell, TableBody } from 'grommet'
-import { useTable } from 'react-table'
+import {
+  Box,
+  Table,
+  TableHeader,
+  TableRow,
+  TableCell,
+  TableBody
+} from 'grommet'
+import { useTable, useGlobalFilter } from 'react-table'
+import { Row } from 'components/shared/Row'
+import { GlobalFilter } from 'components/shared/GlobalFilter'
 
 export const DataTable = ({ columns, data }) => {
-  const tableInstance = useTable({
-    columns,
-    data
-  })
+  const tableInstance = useTable(
+    {
+      columns,
+      data
+    },
+    useGlobalFilter
+  )
 
-  const { getTableProps, headerGroups, getTableBodyProps, rows, prepareRow } =
-    tableInstance
+  const {
+    getTableProps,
+    headerGroups,
+    getTableBodyProps,
+    rows,
+    prepareRow,
+    state: { globalFilter },
+    setGlobalFilter
+  } = tableInstance
 
   return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <Table {...getTableProps()}>
-      <TableHeader>
-        {headerGroups.map((headerGroup) => (
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          <TableRow {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
+    <>
+      <Row margin={{ bottom: 'small' }}>
+        <Box />
+        <GlobalFilter
+          globalFilter={globalFilter}
+          setGlobalFilter={setGlobalFilter}
+        />
+      </Row>
+      <Box
+        border={{ color: 'gray-shade-40', side: 'right' }}
+        style={{ overflow: 'hidden' }}
+      >
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Table {...getTableProps()}>
+          <TableHeader>
+            {headerGroups.map((headerGroup) => (
               // eslint-disable-next-line react/jsx-props-no-spreading
-              <TableCell {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </TableCell>
+              <TableRow {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  <TableCell {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                  </TableCell>
+                ))}
+              </TableRow>
             ))}
-          </TableRow>
-        ))}
-      </TableHeader>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <TableBody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row)
+          </TableHeader>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <TableBody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row)
 
-          return (
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            <TableRow {...row.getRowProps()}>
-              {row.cells.map((cell) => (
+              return (
                 // eslint-disable-next-line react/jsx-props-no-spreading
-                <TableCell {...cell.getCellProps()}>
-                  {cell.render('Cell')}
-                </TableCell>
-              ))}
-            </TableRow>
-          )
-        })}
-      </TableBody>
-    </Table>
+                <TableRow {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    <TableCell {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </Box>
+    </>
   )
 }
 
