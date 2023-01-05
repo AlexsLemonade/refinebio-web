e059259fc50d7ad15189281d8df6e60b1a9547f3
nozomione <nozomione.online@gmail.com>
2023-01-05 12:38:44 -0500

    (minor) fix 'crossOrigin=true' warning in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,11 @@ export default function Document() {
           crossOrigin="true"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400&family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
           rel="stylesheet"
