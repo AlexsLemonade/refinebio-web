d9936261a7c054b345121b13fedd96b347db700d
nozomione <nozomione.online@gmail.com>
2023-01-05 16:50:50 -0500

    (minor) Updates include: - move the GlobalFilter component to /shared/DataTable/ - add memo in [experiment].js - fix the useMemo(pass dependiencies) in the context files

--- src/contexts/RefinebioContext.js
+++ src/contexts/RefinebioContext.js
@@ -7,12 +7,15 @@ export const RefinebioContextProvider = ({ children }) => {
   const [email, setEmail] = useLocalStorage('refinebio-user-email', null)
   const [token, setToken] = useLocalStorage('refinebio-api-token', null)
 
-  const value = useMemo(() => ({
-    email,
-    setEmail,
-    token,
-    setToken
-  }))
+  const value = useMemo(
+    () => ({
+      email,
+      setEmail,
+      token,
+      setToken
+    }),
+    [email, setEmail, token, setToken]
+  )
 
   return (
     <RefinebioContext.Provider value={value}>
