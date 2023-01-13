0668069ba0ecb6164e1fd9bb4b1930f9c6ff3f37
nozomione <nozomione.online@gmail.com>
2023-01-13 18:28:10 -0500

    Updates include: - (edit) add a pad to fix a cross-browser bug(observed in Chrome) for the IntersectionObserver and move the handleScroll method to /HorizontalScrollIndicator(from DataTable) - (edit) update /SortByIcon and add border to a selected column(currently ‘isSorted’ item) - (minor) add a fixed value to the data table’s header to prevent it from stretching out, also add font-size 14 to span in /DataTableSticky - (minor) update a vertical-align value ‘baseline’ to ‘middle’ in GlobalStyle to prevent a table element to align at the bottom of a cell - (temp) swap the mock data item in [experiment].js

--- src/components/shared/DataTable/utils/SortByIcon.js
+++ src/components/shared/DataTable/utils/SortByIcon.js
@@ -6,17 +6,19 @@ export const SortByIcon = ({ isSorted, isSortedDesc, margin = 'none' }) => {
   const size = '10px'
 
   return (
-    <Box width="fit-content" margin={margin}>
-      <Icon
-        name="ChevronUp"
-        size={size}
-        color={isSorted && !isSortedDesc ? 'brand' : 'gray-shade-40'}
-      />
-      <Icon
-        name="ChevronDown"
-        size={size}
-        color={isSorted && isSortedDesc ? 'brand' : 'gray-shade-40'}
-      />
+    <Box width="fit-content" justify="center" margin={margin}>
+      {isSorted && !isSortedDesc && (
+        <Icon name="ChevronUp" size={size} color="black" />
+      )}
+      {isSorted && isSortedDesc && (
+        <Icon name="ChevronDown" size={size} color="black" />
+      )}
+      {!isSorted && (
+        <>
+          <Icon name="ChevronUp" size={size} color="gray-shade-40" />
+          <Icon name="ChevronDown" size={size} color="gray-shade-40" />
+        </>
+      )}
     </Box>
   )
 }
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -23,7 +23,7 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         border: 0;
         font-size: 100%;
-        vertical-align: baseline;
+        vertical-align: middle;
     }
     *, ::before, ::after { 
         box-sizing: border-box; 
