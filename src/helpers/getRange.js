f9743275669afb021f77fa9e0bf6a979a08a5063
nozomione <nozomione.online@gmail.com>
2023-01-19 01:02:29 -0500

    add helpers/getRange(taken from the current refine.bio)

---
+++ src/helpers/getRange.js
@@ -0,0 +1,9 @@
+// Returns an array of consecutive numbers from 1 to n (n > 1)
+
+export const getRange = (n) => {
+  const result = []
+  for (let i = 1; i <= n; i += 1) {
+    result.push(i)
+  }
+  return result
+}
