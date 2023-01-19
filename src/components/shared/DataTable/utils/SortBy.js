b97e0e5fd8c66ab633ec2c48e1af1e2a49e45f09
nozomione <nozomione.online@gmail.com>
2023-01-19 18:22:27 -0500

    (edit) optimize the DataTable component and delete its wrapper styled-component

--- src/components/shared/DataTable/utils/SortBy.js
+++ src/components/shared/DataTable/utils/SortBy.js
@@ -2,11 +2,24 @@ import { memo } from 'react'
 import { Box } from 'grommet'
 import { Icon } from 'components/shared/Icon'
 
-export const SortByIcon = ({ isSorted, isSortedDesc, margin = 'none' }) => {
+export const SortByBorder = memo(({ isSorted }) => (
+  <Box
+    background={isSorted ? 'gray-shade-70' : 'transparent'}
+    height="3px"
+    width="100%"
+    style={{
+      position: 'absolute',
+      left: 0,
+      bottom: 0
+    }}
+  />
+))
+
+export const SortBy = ({ isSorted, isSortedDesc }) => {
   const size = '10px'
 
   return (
-    <Box width="fit-content" justify="center" margin={margin}>
+    <Box width="fit-content" justify="center" margin={{ top: '-2px' }}>
       {isSorted && !isSortedDesc && (
         <Icon name="ChevronUp" size={size} color="black" />
       )}
@@ -23,4 +36,4 @@ export const SortByIcon = ({ isSorted, isSortedDesc, margin = 'none' }) => {
   )
 }
 
-export default memo(SortByIcon)
+export default memo(SortBy)
