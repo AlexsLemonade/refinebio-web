7e32a6eceb680c96a9d4ace390d6345766218cb7
nozomione <nozomione.online@gmail.com>
2023-01-21 11:26:33 -0500

    (minor) re-poistion  props to the top

--- src/components/shared/DataTable/utils/Resizer.js
+++ src/components/shared/DataTable/utils/Resizer.js
@@ -4,6 +4,8 @@ import { Box } from 'grommet'
 export const Resizer = ({ isResizing, ...props }) => {
   return (
     <Box
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
       style={{
         boxShadow: 'none',
         cursor: 'col-resize',
@@ -17,8 +19,6 @@ export const Resizer = ({ isResizing, ...props }) => {
       }}
       width={isResizing ? '100%' : '16px'}
       height="100%"
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...props}
     />
   )
 }
