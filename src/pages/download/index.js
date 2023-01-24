b7d510dc6737bb94b1f6385e6b4fe0b1757b2309
nozomione <nozomione.online@gmail.com>
2023-01-24 12:28:32 -0500

    create the download page(pages/download)

---
+++ src/pages/download/index.js
@@ -0,0 +1,13 @@
+import { memo } from 'react'
+import { Box } from 'grommet'
+import { FixedContainer } from 'components/shared/FixedContainer'
+
+export const Download = () => {
+  return (
+    <Box>
+      <FixedContainer>Download Page</FixedContainer>
+    </Box>
+  )
+}
+
+export default memo(Download)
