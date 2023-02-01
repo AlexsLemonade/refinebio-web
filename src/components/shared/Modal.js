3581582ca8054910e6a3f0ed21df73f5458466f6
nozomione <nozomione.online@gmail.com>
2023-02-01 18:04:04 -0500

    (edit) update the Modal component to simply take a children and without rendering the button for open a modal

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -1,38 +1,27 @@
 import { useModal } from 'hooks/useModal'
-import { Box, Layer, Text } from 'grommet'
+import { Box, Layer } from 'grommet'
 import { Icon } from './Icon'
 import { SrOnly } from './SrOnly'
 
-export const Modal = ({ label, children }) => {
-  const { show, close, open } = useModal()
-
-  const isString = typeof label === 'string'
+export const Modal = ({ children }) => {
+  const { modal, closeModal } = useModal()
 
   return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      <Box
-        role="button"
-        onClick={open}
-        width={isString ? 'fit-content' : 'auto'}
-      >
-        <Text
-          color="brand"
-          style={{
-            textDecoration: isString ? 'underline' : 'none'
-          }}
-        >
-          {label}
-        </Text>
-        <SrOnly label="Open Modal" />
-      </Box>
-      {show && (
-        <Layer onEsc={() => close} onClickOutside={() => close}>
+      {modal.show && (
+        <Layer onEsc={closeModal} onClickOutside={closeModal}>
           <Box>
-            <Box role="button" onClick={close} alignSelf="end">
+            <Box
+              role="button"
+              onClick={closeModal}
+              alignSelf="end"
+              height="24px"
+            >
               <Icon color="gray-shade-70" name="Close" />
               <SrOnly label="Close Modal" />
             </Box>
-            <Box pad={{ top: 'small' }}>{children}</Box>
+            <Box margin={{ top: 'xsmall' }}>{children}</Box>
           </Box>
         </Layer>
       )}
