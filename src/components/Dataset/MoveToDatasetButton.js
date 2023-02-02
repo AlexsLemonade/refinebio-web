8196d3ca08d63d411cfa623f7b95de72f59611ad
nozomione <nozomione.online@gmail.com>
2023-02-02 15:08:04 -0500

    (minor) add the default id value and pass it to openModal method in the ShareDatasetButton and MoveToDatasetButton component

--- src/components/Dataset/MoveToDatasetButton.js
+++ src/components/Dataset/MoveToDatasetButton.js
@@ -2,9 +2,8 @@ import { useModal } from 'hooks/useModal'
 import { Button } from 'components/shared/Button'
 import { Modal } from 'components/shared/Modal'
 
-export const MoveToDatasetButton = ({ id }) => {
+export const MoveToDatasetButton = ({ id = 'move-to-dataset' }) => {
   const { modal, openModal } = useModal()
-  const modalId = 'move-to-dataset'
 
   return (
     <>
@@ -12,7 +11,7 @@ export const MoveToDatasetButton = ({ id }) => {
         label="Move to Dataset"
         secondary
         responsive
-        onClick={() => openModal(modalId)}
+        onClick={() => openModal(id)}
       />
       {modal.id === id && <Modal>Move to Dataset</Modal>}
     </>
