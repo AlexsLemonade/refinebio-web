8e70a286b2ff756a54fe34b9dba3403f46e7a196
nozomione <nozomione.online@gmail.com>
2023-02-06 09:20:52 -0500

    create shared/ExpandableBlock for an expandableable div

---
+++ src/components/shared/ExpandableBlock.js
@@ -0,0 +1,29 @@
+import { Box } from 'grommet'
+
+export const ExpandableBlock = ({
+  dureation = '.8s',
+  expand,
+  opacity = '0.75',
+  maxHeight = '200px',
+  timing = 'cubic-bezier(.61,.36,.24,1.05)',
+  children,
+  ...props
+}) => {
+  return (
+    <Box
+      margin={{ bottom: 'xsmall' }}
+      style={{
+        opacity: expand ? 1 : opacity,
+        overflow: 'hidden',
+        maxHeight: expand ? maxHeight : '0',
+        transition: `all ${dureation} ${timing}`
+      }}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      {children}
+    </Box>
+  )
+}
+
+export default ExpandableBlock
