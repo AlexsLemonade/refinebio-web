48614dc06731193447da9e858f389264d5d433a9
nozomione <nozomione.online@gmail.com>
2023-02-08 10:06:36 -0500

    (minor) fix the comment in helpers/getAPIReadable

--- src/helpers/getAPIReadable.js
+++ src/helpers/getAPIReadable.js
@@ -1,3 +1,3 @@
-// Returns the human-readable text based on the human-readable text value
+// Returns the API readable value based on the human-readable text
 export const getAPIReadable = (value, object) =>
   Object.keys(object).find((key) => object[key] === value)
