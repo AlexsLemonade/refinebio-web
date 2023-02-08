a7b9326c73c80cb39bc473bd6b3db41a04d57ad3
nozomione <nozomione.online@gmail.com>
2023-02-08 13:41:33 -0500

    add a dataset helper 'downloadFilesData' for the file summary (taken from the current refinebio with minor refactoring)

--- src/helpers/dataset/index.js
+++ src/helpers/dataset/index.js
@@ -9,12 +9,35 @@ export const getTotalSamples = (dataset) =>
 // Returns the file size estimates of given dataset and its aggregate_by value (either 'EXPERIMENT' or 'SPECIES')
 // (for Download/DownloadFileSummary)
 export const downloadFilesData = (dataset, samplesBySpecies, aggregateBy) => {
+  const totalExperiments = Object.keys(dataset).length
+
+  // When the samples are aggregated by 'EXPERIMENT'
+  // https://github.com/AlexsLemonade/refinebio-frontend/issues/25#issuecomment-395870627
+  const aggregatedByExperiment = () => ({
+    files: [
+      {
+        title: `${totalExperiments} Gene Expression Matrices`,
+        description: '1 file per Experiment',
+        format: 'tsv'
+      },
+      {
+        title: `${totalExperiments} Sample Metadata Files`,
+        description: '1 file per Experiment',
+        format: 'tsv'
+      },
+      {
+        title: `${totalExperiments} Experiment Metadata Files`,
+        description: '1 file per Experiment',
+        format: 'json'
+      }
+    ]
+  })
+
   // When the samples are aggregated by 'SPECIES'
-  function downloadsFilesDataBySpecies() {
-    const totalExperiments = Object.keys(dataset).length
+  const aggregatedBySpecies = () => {
     const totalSpecies = Object.keys(samplesBySpecies).length
 
-    const data = {
+    return {
       files: [
         {
           title: `${totalSpecies} Gene Expression Matrices`,
@@ -33,39 +56,9 @@ export const downloadFilesData = (dataset, samplesBySpecies, aggregateBy) => {
         }
       ]
     }
-
-    return data
-  }
-
-  // When the samples are aggregated by 'EXPERIMENT'
-  // https://github.com/AlexsLemonade/refinebio-frontend/issues/25#issuecomment-395870627
-  function downloadsFilesDataByExperiment() {
-    const totalExperiments = Object.keys(dataset).length
-
-    const data = {
-      files: [
-        {
-          title: `${totalExperiments} Gene Expression Matrices`,
-          description: '1 file per Experiment',
-          format: 'tsv'
-        },
-        {
-          title: `${totalExperiments} Sample Metadata Files`,
-          description: '1 file per Experiment',
-          format: 'tsv'
-        },
-        {
-          title: `${totalExperiments} Experiment Metadata Files`,
-          description: '1 file per Experiment',
-          format: 'json'
-        }
-      ]
-    }
-
-    return data
   }
 
   return aggregateBy === 'SPECIES'
-    ? downloadsFilesDataBySpecies(dataset, samplesBySpecies)
-    : downloadsFilesDataByExperiment(dataset)
+    ? aggregatedBySpecies(dataset, samplesBySpecies)
+    : aggregatedByExperiment(dataset)
 }
