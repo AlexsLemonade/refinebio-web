664498b7aeaeacc596d8b8fa0b1ddc84fb290108
nozomione <nozomione.online@gmail.com>
2023-02-13 19:50:09 -0500

    (merge) fix merge conflict

--- src/helpers/formatNumbers.js
+++ src/helpers/formatNumbers.js
@@ -1,5 +1,4 @@
 // Formats numbers to a locale string (e.g., 1000 to 1,000)
+export const formatNumners = (nums) => nums.toLocaleString()
 
-export const formatNumbers = (nums) => nums.toLocaleString()
-
-export default formatNumbers
+export default formatNumners
