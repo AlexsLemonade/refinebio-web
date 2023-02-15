81fd1ac778e9c51b86710c9e72f961a9bf9e0c41
nozomione <nozomione.online@gmail.com>
2023-02-14 19:04:06 -0500

    create the ViewSamplesButton component for the view samples modal

---
+++ src/components/Download/DownloadDatasetDetails/views/ViewSamplesButton.js
@@ -0,0 +1,28 @@
+import { useModal } from 'hooks/useModal'
+import { Box } from 'grommet'
+import { Button } from 'components/shared/Button'
+import { Modal } from 'components/shared/Modal'
+
+export const ViewSamplesButton = ({ id, sampleMetadataFields }) => {
+  const { modal, openModal } = useModal()
+
+  return (
+    <>
+      <Button
+        label="View Samples"
+        secondary
+        responsive
+        onClick={() => openModal(id)}
+      />
+      {modal.id === id && (
+        <Modal>
+          {sampleMetadataFields.map((x) => (
+            <Box key={x}>{x}</Box>
+          ))}
+        </Modal>
+      )}
+    </>
+  )
+}
+
+export default ViewSamplesButton
