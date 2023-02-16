21058fda7148c304f777c9a1b4e9afc6695e25f2
nozomione <nozomione.online@gmail.com>
2023-02-16 12:05:51 -0500

    (edit) update helpers/getURLForAccessionCode to use the template literals and remove variables

--- src/helpers/getURLForAccessionCode.js
+++ src/helpers/getURLForAccessionCode.js
@@ -1,13 +1,18 @@
 // Returns the external accession code URL based on a given accession code
 export function getURLForAccessionCode(accessionCode) {
   if (!accessionCode) return ''
-  let mainUrl = ''
+
   if (accessionCode.startsWith('GSE')) {
-    mainUrl = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc='
-  } else if (accessionCode.startsWith('RP', 1)) {
-    mainUrl = 'https://www.ebi.ac.uk/ena/data/view/'
-  } else if (accessionCode.startsWith('E-')) {
-    mainUrl = 'https://www.ebi.ac.uk/arrayexpress/experiments/'
+    return `https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=${accessionCode}`
   }
-  return mainUrl + accessionCode
+
+  if (accessionCode.startsWith('RP', 1)) {
+    return `https://www.ebi.ac.uk/ena/data/view/${accessionCode}`
+  }
+
+  if (accessionCode.startsWith('E-')) {
+    return `https://www.ebi.ac.uk/arrayexpress/experiments/${accessionCode}`
+  }
+
+  return ''
 }
