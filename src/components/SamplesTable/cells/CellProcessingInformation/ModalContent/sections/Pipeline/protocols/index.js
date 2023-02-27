f15839a302d2ee975104bf17b17b52ea1256a577
nozomione <nozomione.online@gmail.com>
2023-02-27 18:59:04 -0500

    Updates include: - create the Pipeline component (Pipeline/sub-components) - VersionInformation     - ProcessorVersion     - VersionList     - VersionItem - PipelineSteps - protocols/     - AffymetrixScan     - IlluminaScan     - Salmon     - Tximport

---
+++ src/components/SamplesTable/cells/CellProcessingInformation/ModalContent/sections/Pipeline/protocols/index.js
@@ -0,0 +1,5 @@
+export { AffymetrixScan } from './AffymetrixScan'
+export { IlluminaScan } from './IlluminaScan'
+export { Salmon } from './Salmon'
+export { SubmitterSupplied } from '../../SubmitterSupplied'
+export { Txtimport } from './Txtimport'
