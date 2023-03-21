1dcf49f95593c98e813ae8b022785482058c9e1f
nozomione <nozomione.online@gmail.com>
2023-03-21 12:22:57 -0400

    create the context for the filter ‘FilterContext’ and hooks/useFilter (to make the filter state sharable in child components) and add its provider in _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,10 +5,12 @@ import { Grommet } from 'grommet'
 import { Layout } from 'components/Layout'
 import { theme } from 'themes'
 import { BandContextvProvider } from 'contexts/BandContext'
-import { RefinebioContextProvider } from 'contexts/RefinebioContext'
-import ErrorPage from 'pages/_error'
-import { ModalContextProvider } from 'contexts/ModalContext'
 import { DatasetContextProvider } from 'contexts/DatasetContext'
+import { FilterContextProvider } from 'contexts/FilterContext'
+import { ModalContextProvider } from 'contexts/ModalContext'
+import { RefinebioContextProvider } from 'contexts/RefinebioContext'
+
+import ErrorPage from 'pages/_error'
 
 const Fallback = () => <ErrorPage />
 
@@ -20,14 +22,16 @@ const App = ({ Component, pageProps }) => {
         <RefinebioContextProvider>
           <DatasetContextProvider>
             <BandContextvProvider>
-              <Layout>
-                <Sentry.ErrorBoundary fallback={Fallback} showDialog>
-                  <ModalContextProvider>
-                    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-                    <Component {...pageProps} />
-                  </ModalContextProvider>
-                </Sentry.ErrorBoundary>
-              </Layout>
+              <FilterContextProvider>
+                <Layout>
+                  <Sentry.ErrorBoundary fallback={Fallback} showDialog>
+                    <ModalContextProvider>
+                      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                      <Component {...pageProps} />
+                    </ModalContextProvider>
+                  </Sentry.ErrorBoundary>
+                </Layout>
+              </FilterContextProvider>
             </BandContextvProvider>
           </DatasetContextProvider>
         </RefinebioContextProvider>
