e6093d3b1fb023d1ceec99b4ebd2f3028fd951b1
nozomione <nozomione.online@gmail.com>
2023-04-03 17:06:40 -0400

    (edit) update the helpers/dataset/*

---
+++ src/helpers/dataset/getTotalSamples.js
@@ -0,0 +1,6 @@
+import isEmptyObject from 'helpers/isEmptyObject'
+import unionizeArrays from 'helpers/unionizeArrays'
+
+// Returns the total length of samples added in My Dataset
+export default (dataset) =>
+  isEmptyObject(dataset) ? 0 : unionizeArrays(...Object.values(dataset)).length
