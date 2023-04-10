355f2679a78a93ef010e7361a978332e88c28d3b
nozomione <nozomione.online@gmail.com>
2023-04-10 15:39:23 -0400

    (edit) refactor helpers/formatString based on the PR feedback

--- src/helpers/formatString.js
+++ src/helpers/formatString.js
@@ -1,9 +1,6 @@
-import { isEmptyStr } from 'helpers/isEmptyStr'
 // Returns a formatted string
 // e.g) 'HOMO_SAPIENS' to 'Homo sapiens'
-
-export function formatString(key) {
-  const temp = key.toLowerCase().replace(/_/g, ' ')
-
-  return !isEmptyStr(key) ? temp.charAt(0).toUpperCase() + temp.slice(1) : null
-}
+export const formatString = ([first, ...rest]) =>
+  [first.toUpperCase(), ...rest.join('').toLowerCase()]
+    .join('')
+    .replace(/_/g, ' ')
