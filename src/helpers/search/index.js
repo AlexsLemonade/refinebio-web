35834a3951bf2a15a8619c853174db7ceeb8ff9e
nozomione <nozomione.online@gmail.com>
2023-04-24 16:50:36 -0400

    add helpers/getAccessionCodesParam (taken from the current refine.bio)

--- src/helpers/search/index.js
+++ src/helpers/search/index.js
@@ -1,3 +1,4 @@
+export { default as getAccessionCodesParam } from './getAccessionCodesParam'
 export { default as getFilterParam } from './getFilterParam'
 export { default as getQueryParam } from './getQueryParam'
 export { default as isChecked } from './isChecked'
