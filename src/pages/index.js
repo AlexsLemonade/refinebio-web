662c0b26b6c8d1f470c292830a7502ecaac58126
nozomione <nozomione.online@gmail.com>
2023-04-26 09:48:19 -0400

    TEMP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import { Heading } from 'grommet'
+import Refinebio from '@ccdl/refinebio/src'
 
 const Home = () => {
+  const api = Refinebio()
+  console.log(api.Dataset())
+
   return (
     <Heading level="1" size="large">
       Home
