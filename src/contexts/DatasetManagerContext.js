39d27eca0ae9ab0086c4de437eee2d3e595c7c69
nozomione <nozomione.online@gmail.com>
2023-04-27 20:36:01 -0400

    (edit) add new states 'datasetIdHistory and processingDatasetIds' and rename 'currentDatasetId' to 'activveDatasetId' in DatasetManagerContext

--- src/contexts/DatasetManagerContext.js
+++ src/contexts/DatasetManagerContext.js
@@ -4,15 +4,35 @@ import { useLocalStorage } from 'hooks/useLocalStorage'
 export const DatasetManagerContext = createContext({})
 
 export const DatasetManagerContextProvider = ({ children }) => {
-  const [currentDatasetId, setCurrentDatasetId] = useLocalStorage(
-    'refinebio-currentDatasetId',
+  // currently selected dataset id
+  const [activeDatasetId, setActiveDatasetId] = useLocalStorage(
+    'active-dataset-id',
     null
+  )
+
+  // a history of user-created dataset ids
+  const [datasetIdHistory, setDatasetHistoryId] = useLocalStorage(
+    'dataset-id-history',
+    []
+  )
+
+  // any dataset ids currenly being processed
+  const [processingDatasetIds, setProcessingDatasetIds] = useLocalStorage(
+    'processing-dataset-ids',
+    []
   )
 
   return (
     <DatasetManagerContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
-      value={{ currentDatasetId, setCurrentDatasetId }}
+      value={{
+        activeDatasetId,
+        setActiveDatasetId,
+        datasetIdHistory,
+        setDatasetHistoryId,
+        processingDatasetIds,
+        setProcessingDatasetIds
+      }}
     >
       {children}
     </DatasetManagerContext.Provider>
