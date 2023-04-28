27115f68995929f473076e7e60064f6ed097e715
nozomione <nozomione.online@gmail.com>
2023-04-28 18:24:00 -0400

    Updates include: (useDatasetManager) - add necessary new states:     - activeDatasetId, setActiveDatasetId     - setDatasetHistoryId     - processingDatasetIds, setProcessingDatasetIds - add new hook’s internal states and remove no longer used states     - processingDatasets, setProcessingDatasets - add the following methods and its implementation (improve later as necessary):     - updateProcessingDatasets     - getProcessingExperimentDataset     - hasData     - formatExperiments - define the following methods without implementation     - formatSampleMetadata     - getDownloadFileCount - adjust the codebase and update/add comments

--- src/hooks/useDatasetManager.js
+++ src/hooks/useDatasetManager.js
@@ -1,7 +1,8 @@
-import { useContext, useMemo, useState, useEffect } from 'react'
+import { useContext, useEffect, useMemo, useState } from 'react'
 import { DatasetManagerContext } from 'contexts/DatasetManagerContext'
 import { useRefinebioContext } from 'hooks/useRefinebioContext'
-import { useLocalStorage } from 'hooks/useLocalStorage'
+import { useInterval } from 'hooks/useInterval'
+import moment from 'moment'
 import unionizeArrays from 'helpers/unionizeArrays'
 import Refinebio from '@ccdl/refinebio/src'
 
@@ -9,14 +10,20 @@ import Refinebio from '@ccdl/refinebio/src'
 export const useDatasetManager = () => {
   const { email, token, setToken } = useRefinebioContext()
   const api = useMemo(() => Refinebio(), [])
-  const { currentDatasetId, setCurrentDatasetId } = useContext(
-    DatasetManagerContext
-  )
-  // eslint-disable-next-line no-unused-vars
-  const [downloads, setDownloads] = useLocalStorage('refinebio-downloads', null) // One-off downloads dataset ids
-  const [currentDataset, setCurrentDataset] = useState(null) // publicInterface methods
+  const {
+    activeDatasetId,
+    setActiveDatasetId,
+    setDatasetHistoryId,
+    processingDatasetIds,
+    setProcessingDatasetIds
+  } = useContext(DatasetManagerContext)
 
-  // Refreshes currentDataset on currentDatasetId changes
+  // stores the latest publicInterface instance
+  const [currentDataset, setCurrentDataset] = useState(null)
+  // stores the last API response for datasets currently being processed
+  const [processingDatasets, setProcessingDatasets] = useState([])
+
+  // refreshes currentDataset on activeDatasetId changes
   useEffect(() => {
     const getNewDataset = async () => {
       const newDataset = await getCurrentDataset()
@@ -24,26 +31,91 @@ export const useDatasetManager = () => {
     }
 
     getNewDataset()
-  }, [currentDatasetId])
+  }, [activeDatasetId])
+
+  const updateProcessingDatasets = async () => {
+    // don't make api call when no processingDatasetIds
+    if (processingDatasetIds.length === 0) return
+
+    const fetchProcessingDataset = async (id) => {
+      const dataset = await getDataset(id)
+
+      return dataset
+    }
+
+    const datasets = await Promise.all(
+      processingDatasetIds.map(fetchProcessingDataset)
+    )
+
+    // removes any processed dataset ids from processingDatasetIds AFTER 1 day
+    // and filter them out from the API response
+    const filteredDatasets = []
+    for (const dataset of datasets) {
+      if (dataset.is_processed) {
+        if (moment.utc().diff(moment(dataset.last_modified, 'days')) < 1) {
+          filteredDatasets.push(dataset)
+        } else {
+          setProcessingDatasetIds((prev) =>
+            prev.filter((id) => id !== dataset.id)
+          )
+        }
+      } else {
+        filteredDatasets.push(dataset)
+      }
+    }
+
+    setProcessingDatasets(filteredDatasets)
+  }
+
+  // listens for changes in processingDatasets
+  // polls the most recent API data every 5 seconds
+  // once the process is finished, removes them from processingDatasets
+  // regardless of whether there are processingDatasetIds,
+  // starts the interval when the component is mounted.
+  useInterval(updateProcessingDatasets, 5000)
 
   /* --- Internal methods --- */
-  // Dataset
-  // Creates a new dataset and returns publicInterface methods
+  /* --- Dataset --- */
+  // formats each element in the API response's 'experiments' array into
+  // an object with the experiment 'accession_code' as its key.
+  // only for details=true
+  const formatExperiments = (experiments) => {
+    if (!experiments.length) return []
+
+    return experiments.reduce(
+      (acc, experiment) => ({
+        ...acc,
+        [experiment.accession_code]: experiment
+      }),
+      {}
+    )
+  }
+
+  // creates a new dataset and returns publicInterface
   const createDataset = async (setCurrent = true) => {
     const dataset = await saveDataset(api.Dataset())
+    // stores the newly created dataset id to a user's browser
+    setDatasetHistoryId(dataset.get('id'))
 
-    if (setCurrent) setCurrentDatasetId(dataset.get('id'))
+    if (setCurrent) setActiveDatasetId(dataset.get('id'))
 
     return dataset
   }
 
-  // Fetchs the latest version of the dataset
+  // fetchs the latest version of the dataset
   const getDataset = async (id, details = false) => {
     let dataset
     const { isOk, response } = await api.dataset.get(id, details)
 
     if (isOk) {
-      dataset = await api.Dataset(response)
+      dataset = api.Dataset(
+        details
+          ? {
+              ...response,
+              experiments: formatExperiments(response.experiments)
+            }
+          : response
+      )
 
       return dataset
     }
@@ -53,18 +125,18 @@ export const useDatasetManager = () => {
     return dataset
   }
 
-  // Returns the latest version of the dataset
+  // returns the latest version of the dataset
   // or create a new one
   const getCurrentDataset = async () => {
     let dataset
 
-    if (!currentDatasetId) {
+    if (!activeDatasetId) {
       dataset = await createDataset()
 
       return dataset
     }
 
-    dataset = await getDataset(currentDatasetId, true)
+    dataset = await getDataset(activeDatasetId, true)
 
     return dataset
   }
@@ -83,12 +155,18 @@ export const useDatasetManager = () => {
     return dataset
   }
 
-  // Updates currentDataset with a newly created reference
+  // updates currentDataset with a newly created reference
   const updateCurrentDataset = (newDataset) =>
     setCurrentDataset({ ...newDataset })
 
-  // Empty the data object in the dataset
-  const clearDataset = async () => {
+  /* Common methods for UI */
+  /* --- Dataset --- */
+
+  // checks whether any data has been added to My Dataset
+  const hasData = (dataset) => Object.keys(dataset).length > 0
+
+  // empty the data object in currently selected dataset
+  const removeAllDataset = async () => {
     const dataset = await currentDataset.emptyData()
     await dataset.save()
     updateCurrentDataset(dataset)
@@ -96,8 +174,21 @@ export const useDatasetManager = () => {
     return dataset
   }
 
-  /* --- Common methods for UI --- */
-  // Experiment
+  /* --- Experiment --- */
+  // returns the lastest state of the dataset being processed by experimentAccessionCode
+  // for one-off download
+  const getProcessingExperimentDataset = (experimentAccessionCode) => {
+    // filter out the dataset that has more than one experiments
+    // and has no experimentAccessionCode
+    const experimentDataset = processingDatasets.filter(
+      (dataset) =>
+        Object.keys(dataset.data).length === 1 &&
+        experimentAccessionCode in dataset.data
+    )
+
+    return experimentDataset
+  }
+
   const getTotalExperiments = () =>
     Object.keys(currentDataset.getData() || {}).length
 
@@ -105,7 +196,7 @@ export const useDatasetManager = () => {
     Object.keys(currentDataset.getData()).includes(experimentAccessionCode)
 
   const addExperiment = async (experimentAccessionCode) => {
-    if (!currentDatasetId) {
+    if (!activeDatasetId) {
       await createDataset()
     }
 
@@ -120,7 +211,7 @@ export const useDatasetManager = () => {
     updateCurrentDataset(dataset)
   }
 
-  // Sample
+  /* --- Sample --- */
   const getTotalSamples = () => {
     const data = currentDataset.getData() || {}
 
@@ -146,14 +237,14 @@ export const useDatasetManager = () => {
     updateCurrentDataset(dataset)
   }
 
-  // For One-off Experiment Downloads
+  /* --- One-off Experiment Downloads --- */
   const downloadExperiment = async (experimentAccessionCode, options) => {
     const dataset = await api.Dataset(options)
     dataset.addExperiment(experimentAccessionCode)
     dataset.setEmail(email)
     await processDataset(dataset)
-    // Stores the newly created dataset id to downloads
-    setDownloads((prev) => unionizeArrays(prev, dataset.get('id')))
+    // add the dataset id to processingDatasetIds
+    setProcessingDatasetIds((prev) => [...prev, dataset.get('id')])
 
     return dataset
   }
@@ -163,7 +254,7 @@ export const useDatasetManager = () => {
   const setDownloadOption = (option, value) =>
     currentDataset.setOption(option, value)
 
-  // Create a token if none and start the download process
+  // creates a token if none and start the download process
   const processDataset = async (dataset) => {
     if (!token) {
       const response = await api.token.create({ is_activated: true })
@@ -176,12 +267,20 @@ export const useDatasetManager = () => {
     return dataset
   }
 
+  /* --- Others --- */
+  // formats sample_metadata names for UI (e.g., 'specimen_part' to 'Specimen part')
+  // eslint-disable-next-line no-unused-vars
+  const formatSampleMetadata = (sampleMtadata) => {}
+
+  // returns the file count estimates of given dataset and its aggregate_by value (either 'EXPERIMENT' or 'SPECIES')
+  // (for Download/DownloadFileSummary)
+  // eslint-disable-next-line no-unused-vars
+  const getDownloadFileCount = (dataset, samplesBySpecies, aggregateBy) => {}
+
   return {
-    currentDatasetId,
-    setCurrentDatasetId,
-    clearDataset,
-    createDataset,
-    getDataset,
+    hasData,
+    removeAllDataset,
+    getProcessingExperimentDataset,
     getTotalExperiments,
     hasExperiment,
     addExperiment,
@@ -192,6 +291,8 @@ export const useDatasetManager = () => {
     removeSample,
     downloadExperiment,
     getDownloadOption,
-    setDownloadOption
+    setDownloadOption,
+    formatSampleMetadata,
+    getDownloadFileCount
   }
 }
