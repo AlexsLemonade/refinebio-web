3aeed3f8b0f2ffdb3f13b3e1f082083756c3a581
nozomione <nozomione.online@gmail.com>
2023-04-27 20:34:43 -0400

    (minor) clean up the state datasetId from RefinebioContext (no longer used)

--- src/contexts/RefinebioContext.js
+++ src/contexts/RefinebioContext.js
@@ -5,13 +5,12 @@ import { DatasetManagerContextProvider } from 'contexts/DatasetManagerContext'
 export const RefinebioContext = createContext({})
 
 export const RefinebioContextProvider = ({ children }) => {
-  const [datasetId, setDatasetId] = useLocalStorage('refinebio-datasetId', null)
   const [email, setEmail] = useLocalStorage('refinebio-user-email', null)
   const [token, setToken] = useLocalStorage('refinebio-api-token', null)
 
   const value = useMemo(
-    () => ({ datasetId, setDatasetId, email, setEmail, token, setToken }),
-    [datasetId, setDatasetId, email, setEmail, token, setToken]
+    () => ({ email, setEmail, token, setToken }),
+    [email, setEmail, token, setToken]
   )
 
   return (
