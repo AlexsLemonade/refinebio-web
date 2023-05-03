14e021d1cb2f7009b6923c5a0a3f6c7ee037f5bc
nozomione <nozomione.online@gmail.com>
2023-05-03 15:47:00 -0400

    (minor) edit comments in helpers

--- src/helpers/setEdgeSizes.js
+++ src/helpers/setEdgeSizes.js
@@ -1,6 +1,5 @@
 // Generates the Grommet `edgeSize` object(in themes/global)
 // using the specified base unit(base) and range values(min, max)
-
 export const setEdgeSizes = (base, min, max) =>
   Object.fromEntries(
     Array.from(Array(max - min + 1).keys()).map((x) => [
