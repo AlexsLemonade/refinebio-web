0e4d730cbcce9db3b89d281242b533765af93e74
nozomione <nozomione.online@gmail.com>
2023-05-25 13:37:01 -0400

    create the sitemap.config file which includes necessary properties for generating sitemap files (matches current refine.bio)

---
+++ sitemap.config.js
@@ -0,0 +1,22 @@
+const apiVersion = process.env.API_VERSION || 'v1'
+const apiPath = process.env.API_PATH || `https://api.refine.bio/${apiVersion}`
+const hostname = process.env.HOST_NAME || 'https://www.refine.bio'
+const limit = 3 // TEMPORARY for development purpose
+
+const sitemap = {
+  apiVersion,
+  apiPath,
+  endpoints: {
+    experiments: `${apiPath}/search/?limit=${limit}&ordering=id`
+  },
+  hostname,
+  outDir: `${__dirname}/public`,
+  resourceNames: ['experiments'],
+  staticPaths: ['/', '/about', '/license', '/privacy', '/terms'],
+  filePrefix: 'sitemap',
+  baseConfig: {
+    limit: 50000 // defaults to 45k
+  }
+}
+// supports CommonJS exports (used in sitemap.js)
+module.exports = sitemap
