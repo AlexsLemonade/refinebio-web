0787554830e3cbea892a15b60e652ed456879d08
nozomione <nozomione.online@gmail.com>
2023-06-13 07:51:01 -0400

    (edit) add the reference for the search page to the View Samples button (in SearchCardFooter) using useSearchManager hook

--- src/components/SearchCard/SearchCardFooter.js
+++ src/components/SearchCard/SearchCardFooter.js
@@ -1,14 +1,22 @@
+import { useRouter } from 'next/router'
+import { useSearchManager } from 'hooks/useSearchManager'
 import formatURLString from 'helpers/formatURLString'
 import { Box } from 'grommet'
 import { Button } from 'components/shared/Button'
 
 export const SearchCardFooter = ({ accessionCode, title }) => {
+  const { push } = useRouter()
+  const { search, setSearch } = useSearchManager()
+
   return (
     <Box>
       <Button
-        href={`experiments/${accessionCode}/${formatURLString(title)}`}
         label="View Samples"
         secondary
+        onClick={() => {
+          push(`/experiments/${accessionCode}/${formatURLString(title)}`)
+          setSearch({ ...search, ref: 'search' })
+        }}
       />
     </Box>
   )
