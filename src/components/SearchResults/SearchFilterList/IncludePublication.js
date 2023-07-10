4d71ba75d92e238f54862dc82b5369dddc2e2e90
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-07-10 16:29:37 -0400

    (edit) disable the include publication checkbox when no publication

--- src/components/SearchResults/SearchFilterList/IncludePublication.js
+++ src/components/SearchResults/SearchFilterList/IncludePublication.js
@@ -10,15 +10,15 @@ export const IncludePublication = ({
 }) => {
   const { isFilterChecked, toggleFilter } = useSearchManager()
   const { viewport } = useResponsive()
-  const count =
-    filterGroup && filterGroup.true
-      ? `(${formatNumbers(filterGroup.true)})`
-      : ''
+  const count = `(${
+    filterGroup && filterGroup.true ? formatNumbers(filterGroup.true) : 0
+  })`
 
   return (
     <Box>
       <CheckBox
         checked={isFilterChecked(filterOption)}
+        disabled={!filterGroup.true}
         label={`${filterLabel} ${count}`}
         onChange={(e) =>
           toggleFilter(
