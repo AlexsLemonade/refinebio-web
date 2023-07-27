c3676d4f4be443105c9b2cec332e0e9d9c1b8ac7
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-07-27 12:01:59 -0400

    (edit) import PageTitle in necessary pages

--- src/pages/compendia/[type].js
+++ src/pages/compendia/[type].js
@@ -1,21 +1,25 @@
 import { useResponsive } from 'hooks/useResponsive'
 import { Box } from 'grommet'
 import { CompendiaHero, CompendiaTabs } from 'components/Compendia'
+import { PageTitle } from 'components/shared/PageTitle'
 import { SignUpBlock } from 'components/shared/SignUpBlock'
 
 export const Compendia = () => {
   const { setResponsive } = useResponsive()
 
   return (
-    <Box
-      pad={{
-        top: setResponsive('basex7', 'basex7', 'basex10')
-      }}
-    >
-      <CompendiaHero />
-      <CompendiaTabs />
-      <SignUpBlock />
-    </Box>
+    <>
+      <PageTitle title="Compendia" noSuffix />
+      <Box
+        pad={{
+          top: setResponsive('basex7', 'basex7', 'basex10')
+        }}
+      >
+        <CompendiaHero />
+        <CompendiaTabs />
+        <SignUpBlock />
+      </Box>
+    </>
   )
 }
 
