3ccb9767d658387ffa5f32fefc4d595423a337dd
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-07-27 12:00:45 -0400

    create the PageTitle shared component

---
+++ src/components/shared/PageTitle.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+export const PageTitle = ({ title = '', noSuffix = false }) => {
+  const router = useRouter()
+  const [path, setPath] = useState('')
+
+  useEffect(() => {
+    setPath(router.asPath)
+  }, [router, path])
+
+  const suffix = 'refine.bio'
+  let pageTitle = ''
+
+  switch (true) {
+    case title.length > 0:
+      pageTitle = `${title}`
+      break
+    case /\/$/.test(path):
+      pageTitle = `Search for harmonized transcriptome data -`
+      break
+    case /\/about$/.test(path):
+      pageTitle = `About`
+      break
+    case /\/dataset/.test(path):
+      pageTitle = `Dataset -`
+      break
+    case /\/download/.test(path):
+      pageTitle = `Download -`
+      break
+    case /\/license$/.test(path):
+      pageTitle = `License -`
+      break
+    case /\/privvacy$/.test(path):
+      pageTitle = `Privacy -`
+      break
+    case /\/terms$/.test(path):
+      pageTitle = `Terms of Use -`
+      break
+    default:
+      break
+  }
+
+  return (
+    <Head>
+      <title>
+        {pageTitle}
+        {noSuffix ? '' : ` ${suffix}`}
+      </title>
+    </Head>
+  )
+}
+
+export default PageTitle
