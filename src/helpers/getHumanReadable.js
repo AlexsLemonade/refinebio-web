80e56af0675b6fe657570a154efd910865db5757
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-02 21:23:45 -0400

    clean up

--- src/helpers/getHumanReadable.js
+++
@@ -1,2 +0,0 @@
-// Returns the human-readable text based on the API key
-export const getHumanReadable = (key, object) => object[key] || key
--- src/hooks/useSampleDebug.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react'
-import { SampleDebugContext } from 'contexts/SampleDebugContext'
-
-export const useSampleDebug = () => useContext(SampleDebugContext)
