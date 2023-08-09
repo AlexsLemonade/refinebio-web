6800a6d09e97d35461b571ea431616ca4ce1d288
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-09 17:40:58 -0400

    (migration) resolve the error 'UnhandledSchemeError' for node: prefix for Webpack 5 (resources) - https://github.com/vercel/next.js/issues/28774 - https://webpack.js.org/plugins/normal-module-replacement-plugin - https://github.com/storybookjs/storybook/blob/7dc255b7e99f2f1837c61efdad5e766e4254ee98/docs/addons/writing-presets.md\#webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 import * as path from 'path'
 
 export default {
-  stories: ['./stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['./stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-links',
@@ -19,15 +19,21 @@ export default {
   docs: {
     autodocs: true
   },
-  webpackFinal: async (config) => {
+  webpackFinal: async (config, { presets }) => {
     // (resource) 'configType' https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-
+    const webpack = await presets.apply('webpackInstance')
     config.resolve.modules.push(path.resolve(__dirname, './../src'))
     config.resolve.alias['utils'] = path.resolve(__dirname, './utils')
     config.resolve.alias['unfetch'] = path.resolve(
       __dirname,
       './../node_modules/unfetch/dist/unfetch.mjs'
     )
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, '')
+      })
+    )
+
     // Return the altered config
     return config
   }
