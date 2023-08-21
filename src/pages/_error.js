b10782da2711a93a49e9a8ec1e63908f8cacf8f3
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-21 17:07:38 -0400

    (edit) import the custom 505 component in _error and temporarily return 500 for demo

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,11 +1,13 @@
 import { FixedContainer } from 'components/shared/FixedContainer'
 import { Custom404 } from 'components/Error/Custom404'
+import { Custom500 } from 'components/Error/Custom500'
 import { HeavyTraffic } from 'components/Error/HeavyTraffic'
 
 export const ErrorPage = ({ statusCode }) => {
   return (
     <FixedContainer>
       {statusCode === 404 && <Custom404 />}
+      {statusCode === 500 && <Custom500 />}
       {statusCode === 504 && <HeavyTraffic />}
     </FixedContainer>
   )
@@ -13,7 +15,7 @@ export const ErrorPage = ({ statusCode }) => {
 
 ErrorPage.getInitialProps = async ({ err }) => {
   return {
-    statusCode: err ? err.statusCode : 504
+    statusCode: err ? err.statusCode : 500
   }
 }
 
