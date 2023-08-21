aacb0e1c4bef47400dd7ebe23e44b5dd71ac634e
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-21 16:20:27 -0400

    (edit) import the heavy traffic error view in _error and add the conditions

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,8 +1,14 @@
 import { FixedContainer } from 'components/shared/FixedContainer'
 import { Custom404 } from 'components/Error/Custom404'
+import { HeavyTraffic } from 'components/Error/HeavyTraffic'
 
 export const ErrorPage = ({ statusCode }) => {
-  return <FixedContainer>{statusCode === 404 && <Custom404 />}</FixedContainer>
+  return (
+    <FixedContainer>
+      {statusCode === 404 && <Custom404 />}
+      {statusCode === 504 && <HeavyTraffic />}
+    </FixedContainer>
+  )
 }
 
 ErrorPage.getInitialProps = async ({ res, err }) => {
