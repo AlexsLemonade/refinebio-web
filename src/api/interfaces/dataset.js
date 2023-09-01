69bf4acfd407ee3d85986f9217fa2841f87c710e
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-09-01 11:34:34 -0400

    (edit) update the dataset api interface

--- src/api/interfaces/dataset.js
+++ src/api/interfaces/dataset.js
@@ -3,7 +3,8 @@ import http from 'api/http'
 const url = 'dataset/'
 
 export default {
-  get: (id, params) => http.get(`${url}${id}/`, params),
+  create: (params) => http.post(url, params),
+  get: (id, params, headers) => http.get(`${url}${id}/`, params, headers),
   update: (id, params, details) =>
     http.put(`${url}${id}/${details ? '?details=true' : ''}`, params)
 }
