2676b98a3059d3e9fb2d6be0e779300527fa64d8
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-09-14 17:19:43 -0400

    create the helpers 'intersectArrays' and 'hasSameElements' for useDatasetAction hook

---
+++ src/helpers/hasSameElements.js
@@ -0,0 +1,7 @@
+import differenceOfArrays from 'helpers/differenceOfArrays'
+// Returns true if both given arguments have the same elements, otherwise false
+// e.g., ([1,2], [2,1]) === true
+
+export default (arr1, arr2) =>
+  differenceOfArrays(arr1, arr2).length === 0 &&
+  differenceOfArrays(arr2, arr1).length === 0
