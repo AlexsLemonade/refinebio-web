10578365d0d07a6403564b9314214115c6524ed7
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-09-22 15:19:09 -0400

    create the validation schema for the download email form in config/validationSchemas

---
+++ src/config/validationSchemas.js
@@ -0,0 +1,19 @@
+import * as yup from 'yup'
+
+const DownloadEmailForm = yup.object().shape({
+  email: yup
+    .string()
+    .email('Please enter a vaild email')
+    .required('Please enter your email address'),
+  termsOfService: yup
+    .boolean()
+    .oneOf(
+      [true],
+      'Please accept our terms of use to process and download data'
+    ),
+  receiveUpdates: yup.boolean()
+})
+
+export default {
+  DownloadEmailForm
+}
