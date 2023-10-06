90542f5c13164bab96fe475e67b7c6ea33a07555
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-10-06 15:22:54 -0400

    179 - Implement useDatasetAction hook for dataset action button (#183)
    * create the helpers 'intersectArrays' and 'hasSameElements' for useDatasetAction hook
    
    * create the hook useDatasetAction
    
    * (NOTE: the same update was committed in the previous PR) rename the helpers 'getDifferenceOfArrays' to shorter name 'differenceOfArrays' and minor refactor
    
    * (minor fix) update the import for the helper 'differenceOfArrays' in useDatasetManager
    
    * (minor) fix typos in comments, make the method name more explicit a small optimization
    
    * rename totalSamplesInDataset to getTotalSamplesInDataset
    
    * rename allProcessedInDataset to getHasAllProcessed
    
    * rename anyProcessedInDataset to getAnyProcessedInDataset
    
    * rename intersectDatasets to getDatasetIntersection
    
    * apply feedback - rename 'data1' to 'datasetData’, 'data2' to ‘datasetDataChanges’, and 'addedSlice' to 'addedSamples' (not to use the term slice)
    
    * apply review - rename the parameters 'd1' to 'datasetDataOne' and 'd2' to 'datasetDataTwo' in hasSameSamples

---
+++ src/hooks/useDatasetAction.js
@@ -0,0 +1,111 @@
+import hasSameElements from 'helpers/hasSameElements'
+import intersectArrays from 'helpers/intersectArrays'
+import unionizeArrays from 'helpers/unionizeArrays'
+// This hook is used for the dataset action buttons and compares the following datasets:
+// - datasetData: a data object (added processed samples) in a user-created dataset
+// - datasetDataChanges: all processed samples in the experiment selected by a user via the search results page
+export const useDatasetAction = (datasetData = {}, datasetDataChanges = {}) => {
+  // returns true if any downloadable samples in datasetDataChanges
+  const anyProcessedSamples = () =>
+    Object.values(datasetDataChanges).some(
+      (samples) =>
+        (samples && samples.length > 0) ||
+        (samples && samples.all && samples.total > 0)
+    )
+
+  // returns true if all the processed samples in datasetDataChanges were added to datasetData
+  const getHasAllProcessed = () => {
+    if (!anyProcessedSamples()) return false
+
+    const addedSamples = getAddedSamples()
+    return hasSameSamples(addedSamples, datasetDataChanges)
+  }
+
+  // returns true if any processed samples in datasetDataChanges were added in datasetData
+  const getAnyProcessedInDataset = () =>
+    Object.values(getAddedSamples()).some(
+      (samples) => samples && samples.length > 0
+    )
+
+  // returns the samples in datasetDataChanges that were added in datasetData
+  const getAddedSamples = () => getDatasetIntersection()
+
+  // returns all the samples that are in both datasetData and datasetDataChanges
+  const getSamplesInDatasets = () => {
+    const addedSamples = getAddedSamples()
+
+    return unionizeArrays(...Object.values(addedSamples))
+  }
+
+  // returns true if two given datasets have the same samples, otherwise false
+  const hasSameSamples = (datasetDataOne, datasetDataTwo) => {
+    const datasetDataOneKeys = Object.keys(datasetDataOne)
+    const datasetDataTwoKeys = Object.keys(datasetDataTwo)
+
+    if (!hasSameElements(datasetDataOneKeys, datasetDataTwoKeys)) {
+      return false
+    }
+
+    for (const accession of datasetDataOneKeys) {
+      if (datasetDataOne[accession].all) {
+        if (
+          datasetDataOne[accession].total !== datasetDataTwo[accession].length
+        ) {
+          return false
+        }
+        continue
+      }
+      if (datasetDataTwo[accession].all) {
+        if (
+          datasetDataTwo[accession].total !== datasetDataOne[accession].length
+        ) {
+          return false
+        }
+        continue
+      }
+      if (
+        !hasSameElements(datasetDataOne[accession], datasetDataTwo[accession])
+      ) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  // returns the intersection of two given datasets
+  const getDatasetIntersection = () => {
+    const data = {}
+    const experimentAccessions = intersectArrays(
+      Object.keys(datasetData),
+      Object.keys(datasetDataChanges)
+    )
+
+    for (const experimentAccession of experimentAccessions) {
+      if (datasetDataChanges[experimentAccession].all) {
+        data[experimentAccession] = datasetData[experimentAccession]
+      } else if (datasetData[experimentAccession].all) {
+        data[experimentAccession] = datasetDataChanges[experimentAccession]
+      } else {
+        data[experimentAccession] = intersectArrays(
+          datasetData[experimentAccession],
+          datasetDataChanges[experimentAccession]
+        )
+      }
+    }
+    return data
+  }
+
+  // returns the total samples added in datasetData
+  const getTotalSamplesInDataset = () => {
+    return getSamplesInDatasets().length
+  }
+
+  return {
+    anyProcessedSamples,
+    getHasAllProcessed,
+    getAnyProcessedInDataset,
+    getAddedSamples,
+    getTotalSamplesInDataset
+  }
+}
