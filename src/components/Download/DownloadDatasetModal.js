7007eda6df435e521c5d7d447070aaa28b9104d7
Nozomi Ichihara <nozomione.online@gmail.com>
2023-10-05 21:06:08 -0400

    create the DownloadDatasetButton/DwonloadDatasetModal components using useDatasetManager

---
+++ src/components/Download/DownloadDatasetModal.js
@@ -0,0 +1,127 @@
+import { useRouter } from 'next/navigation'
+import { Formik } from 'formik'
+import { Box, Form, Heading, Paragraph } from 'grommet'
+import { validationSchemas } from 'config'
+import { useDatasetManager } from 'hooks/useDatasetManager'
+import { useResponsive } from 'hooks/useResponsive'
+import { Button } from 'components/shared/Button'
+import { AdvanedOptions } from 'components/Download/DownloadOptionsForm/AdvanedOptions'
+import { AggregateOptions } from 'components/Download/DownloadOptionsForm/AggregateOptions'
+import { TransformationOptions } from 'components/Download/DownloadOptionsForm/TransformationOptions'
+import { EmailField } from 'components/Download/EmailForm/EmailField'
+import { ReceiveUpdatesCheckBox } from 'components/Download/EmailForm/ReceiveUpdatesCheckBox'
+import { TermsOfUseCheckBox } from 'components/Download/EmailForm/TermsOfUseCheckBox'
+
+export const DownloadDatasetModal = ({ dataset, id, closeModal }) => {
+  const { push } = useRouter()
+  const { email, startProcessingDataset } = useDatasetManager()
+  const { setResponsive } = useResponsive()
+  const { DownloadEmailForm } = validationSchemas
+
+  return (
+    <Box
+      margin={{ bottom: 'medium' }}
+      pad={{ horizontal: 'large' }}
+      width={setResponsive('100%', '100%', '500px')}
+    >
+      <Heading level={5} weight="500" margin={{ bottom: 'medium' }}>
+        Download Options:
+      </Heading>
+      <Formik
+        initialValues={{
+          aggregate_by: dataset.aggregate_by,
+          data: dataset.data,
+          scale_by: dataset.scale_by,
+          quantile_normalize: dataset.quantile_normalize,
+          emailAddress: email || '',
+          receiveUpdates: true,
+          termsOfUse: false
+        }}
+        validationSchema={DownloadEmailForm}
+        validateOnChange={false}
+        onSubmit={async (values, { setSubmitting }) => {
+          const response = await startProcessingDataset(dataset.id, values)
+          const pathname = `/dataset/${response.id}`
+          push({ pathname }, pathname)
+          closeModal(id)
+          setSubmitting(false)
+        }}
+      >
+        {({
+          errors,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+          touched,
+          values
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <Box margin={{ bottom: 'small' }}>
+              <AggregateOptions
+                value={values.aggregate_by}
+                handleChange={handleChange}
+                column
+              />
+            </Box>
+            <Box margin={{ bottom: 'xsmall' }}>
+              <TransformationOptions
+                value={values.scale_by}
+                handleChange={handleChange}
+                column
+              />
+            </Box>
+            <Box>
+              <AdvanedOptions
+                datasetId={dataset.id}
+                values={values}
+                handleChange={handleChange}
+                toggle
+              />
+            </Box>
+            <Box margin={{ bottom: 'large' }}>
+              <Paragraph>
+                <strong>
+                  Putting the download files together takes about 10-20 minutes.
+                  Enter your email and we will send you the download link once
+                  your files are ready.
+                </strong>
+              </Paragraph>
+              <Box pad={{ top: 'small' }}>
+                <EmailField
+                  error={errors.emailAddress}
+                  touched={touched.emailAddress}
+                  value={values.emailAddress}
+                  handleChange={handleChange}
+                />
+                <TermsOfUseCheckBox
+                  error={errors.termsOfUse}
+                  touched={touched.termsOfUse}
+                  value={values.termsOfUse}
+                  handleChange={handleChange}
+                />
+                <ReceiveUpdatesCheckBox
+                  value={values.receiveUpdates}
+                  handleChange={handleChange}
+                />
+              </Box>
+            </Box>
+            <Box align="end">
+              <Button
+                label="Start Processing"
+                isLoading={isSubmitting}
+                primary
+                responsive
+                margin={{
+                  bottom: setResponsive('small', 'small', 'none')
+                }}
+                type="submit"
+              />
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  )
+}
+
+export default DownloadDatasetModal
