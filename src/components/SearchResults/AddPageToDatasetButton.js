af5150515f9aab6618e2cb8ea4510142865cd4a1
Nozomi Ichihara <nozomione.online@gmail.com>
2023-10-11 19:06:17 -0400

    (edit) use the getFormattedExperimentList to format the API response in the AddPageToDatasetButton (the API response is a list of experiments shown in a page that a user is currently viewing)

--- src/components/SearchResults/AddPageToDatasetButton.js
+++ src/components/SearchResults/AddPageToDatasetButton.js
@@ -1,17 +1,13 @@
+import { useDatasetAction } from 'hooks/useDatasetAction'
 import { useResponsive } from 'hooks/useResponsive'
-import fromPairs from 'helpers/fromPairs'
 import { DatasetActionButton } from 'components/shared/DatasetActionButton'
 
 export const AddPageToDatasetButton = ({ dataToAdd }) => {
+  const { getFormattedExperimentList } = useDatasetAction()
   const { setResponsive } = useResponsive()
 
-  const downloadableSamples = fromPairs(
-    dataToAdd
-      .filter((item) => item.num_downloadable_samples > 0)
-      .map((item) => [
-        item.accession_code,
-        { all: true, total: item.num_downloadable_samples }
-      ])
+  const downloadableSamples = getFormattedExperimentList(
+    dataToAdd.filter((item) => item.num_downloadable_samples > 0)
   )
 
   return (
