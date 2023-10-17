1cbadd238c2e2cd9f47511f6172272b07d48671d
Nozomi Ichihara <nozomione.online@gmail.com>
2023-10-17 18:38:31 -0400

    (edit) remove the dataset prop from ProcessingDatasetPill and ProcessingDatasetPill Modal (instead pass ids in modal) and clean up the temp logic for rendering a user's email address

--- src/components/shared/SearchCard/SearchCardAction/ProcessingDatasetPill.js
+++ src/components/shared/SearchCard/SearchCardAction/ProcessingDatasetPill.js
@@ -1,18 +1,13 @@
 import { Box } from 'grommet'
-import { useRefinebio } from 'hooks/useRefinebio'
-import { useDatasetManager } from 'hooks/useDatasetManager'
 import { useModal } from 'hooks/useModal'
 import { Modal } from 'components/shared/Modal'
 import { Pill } from 'components/shared/Pill'
 import { ProcessingDatasetPillModal } from './ProcessingDatasetPillModal'
 
-// Showing Badge while dataset is processing
-export const ProcessingDatasetPill = ({ dataset }) => {
-  // TEMPORARY
-  const { email } = useRefinebio()
-  const { datasetId } = useDatasetManager()
-  const { closeModal, openModal } = useModal()
-  const id = `procecssing-dataset-${dataset?.id || datasetId}`
+// show the processing pill while one-off experiment is being processed
+export const ProcessingDatasetPill = ({ datasetId }) => {
+  const { openModal } = useModal()
+  const id = `procecssing-dataset-${datasetId}`
 
   return (
     <Modal
@@ -25,11 +20,7 @@ export const ProcessingDatasetPill = ({ dataset }) => {
       fullHeight={false}
       width="520px"
     >
-      <ProcessingDatasetPillModal
-        dataset={dataset}
-        email={email || 'jone.dow@example.com'}
-        closeModal={() => closeModal(id)}
-      />
+      <ProcessingDatasetPillModal datasetId={datasetId} id={id} />
     </Modal>
   )
 }
--- src/components/shared/SearchCard/SearchCardAction/ProcessingDatasetPillModal.js
+++ src/components/shared/SearchCard/SearchCardAction/ProcessingDatasetPillModal.js
@@ -1,14 +1,15 @@
-import { Box, Heading, Paragraph, Text } from 'grommet'
+import { Box, Heading, Paragraph } from 'grommet'
 import { useDatasetManager } from 'hooks/useDatasetManager'
+import { useModal } from 'hooks/useModal'
 import { useResponsive } from 'hooks/useResponsive'
 import getDomain from 'helpers/getDomain'
 import { Anchor } from 'components/shared/Anchor'
 import { Button } from 'components/shared/Button'
 import { IconProcessingGears } from 'components/shared/IconProcessingGears'
 
-export const ProcessingDatasetPillModal = ({ dataset, email, closeModal }) => {
-  // TEMPORARY
-  const { datasetId } = useDatasetManager()
+export const ProcessingDatasetPillModal = ({ datasetId, id }) => {
+  const { email } = useDatasetManager()
+  const { closeModal } = useModal()
   const { setResponsive } = useResponsive()
 
   return (
@@ -24,14 +25,13 @@ export const ProcessingDatasetPillModal = ({ dataset, email, closeModal }) => {
       </Box>
 
       <Paragraph>
-        An email with a download link will be sent to{' '}
-        <Text>{dataset?.email || email}</Text> when the dataset is ready or you
-        can track the status{' '}
+        An email with a download link will be sent to <strong>{email}</strong>{' '}
+        when the dataset is ready or you can track the status{' '}
         <Anchor
           label="here"
-          href={`${getDomain()}/dataset/${dataset?.id || datasetId}`}
-          target="_blank"
-        />{' '}
+          href={`${getDomain()}/dataset/${datasetId}`}
+          target="_self"
+        />
         .
       </Paragraph>
       <Box margin={{ vertical: 'large' }}>
@@ -42,7 +42,7 @@ export const ProcessingDatasetPillModal = ({ dataset, email, closeModal }) => {
           label="Continue Browsing"
           secondary
           responsive
-          onClick={closeModal}
+          onClick={() => closeModal(id)}
         />
       </Box>
     </Box>
