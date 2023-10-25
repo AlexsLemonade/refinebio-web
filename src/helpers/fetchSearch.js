b93d064a52f8df92a2a93d352f5fd19f8cc35add
Nozomi Ichihara <nozomione.online@gmail.com>
2023-10-25 15:56:54 -0400

    (edit) refactor helpers/fetchSearch for accession code results and adjust pages/search, add helpers/getUniqElementBy used in helpers/fetchSearch

--- src/helpers/fetchSearch.js
+++ src/helpers/fetchSearch.js
@@ -1,24 +1,42 @@
 import { api } from 'api'
 import getAccessionCodesQueryParam from './getAccessionCodesQueryParam'
+import getUniqElementBy from './getUniqElementBy'
 
 export default async (queryString) => {
   const response = await api.search.get(queryString)
-  let accessionCodesResponse
+  let { results } = response
+  const { count: totalResults, facets } = response
   const accessionCodes = getAccessionCodesQueryParam(queryString.search)
 
   if (accessionCodes) {
-    accessionCodesResponse =
-      (await Promise.all(
-        accessionCodes.map((code) =>
-          api.search.get({
-            search: [
-              `accession_code:${code}`,
-              `alternate_accession_code:${code}`
-            ]
-          })
-        )
-      )) || []
+    const accessionCodesResponse = await Promise.all(
+      accessionCodes.map((code) =>
+        api.search.get({
+          search: [`accession_code:${code}`, `alternate_accession_code:${code}`]
+        })
+      )
+    )
+
+    const matchedAccessionCodes = []
+      .concat(...accessionCodesResponse.map((data) => data.results))
+      .map((result) => ({
+        ...result,
+        // adds the flag below to accession codes that matches the user's searched accession codes
+        // these are usually from accession code search
+        isMatchedAccessionCode: true
+      }))
+
+    if (matchedAccessionCodes.length > 0) {
+      // removes duplicates due to multiple requests for fetching accession codes
+      results = getUniqElementBy(
+        [...matchedAccessionCodes, ...results],
+        (x) => x.accession_code
+      )
+    } else if (accessionCodes.length === 1) {
+      // returns no result if no match found for a single accession code that users searched for
+      results = []
+    }
   }
 
-  return { response, accessionCodesResponse }
+  return { facets, results, totalResults }
 }
