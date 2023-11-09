711a38ad0de1c145e3cb80b8efbd1da09a69f741
Nozomi Ichihara <nozomione.online@gmail.com>
2023-11-09 11:48:16 -0500

    (edit) add 'types' to specify default settings for each type, 'error', 'info', and 'success' to the InlineMessage Component

--- src/components/shared/InlineMessage.js
+++ src/components/shared/InlineMessage.js
@@ -1,43 +1,58 @@
 import { Box, Paragraph } from 'grommet'
 import { Icon } from 'components/shared/Icon'
 
-// the color prop must be one of ['error', 'info', 'success']
+// supported types: 'error', 'info', 'success'
+const types = {
+  error: {
+    icon: 'Warning', // icon namne
+    color: 'error', // icon color
+    fontColor: 'error' // label font color
+  },
+  info: {
+    icon: 'Info',
+    color: 'info',
+    fontColor: 'black'
+  },
+  success: {
+    icon: 'Success',
+    color: 'success',
+    fontColor: 'success'
+  }
+}
 
 export const InlineMessage = ({
-  align = 'center',
-  color = 'info',
-  fontColor = 'black',
-  fontSize = 'small',
-  height = '24px',
+  type = 'info',
   label = '',
   labelOnly = false,
+  align = 'center',
+  color = '',
+  fontColor = '',
+  fontSize = 'small',
+  height = '24px',
   iconSize = 'medium',
   ...props
-}) => {
-  const SVG =
-    color === 'error'
-      ? 'Warning'
-      : `${color.substring(0, 1).toUpperCase()}${color.substring(1)}`
-
-  return (
-    <Box
-      align={align}
-      direction="row"
-      height={height}
-      width="max-content"
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...props}
-    >
-      {!labelOnly && (
-        <Box margin={{ right: 'xxsmall' }} height={height}>
-          <Icon color={color} name={SVG} size={iconSize} />
-        </Box>
-      )}
-      <Paragraph color={color !== 'info' ? color : fontColor} size={fontSize}>
-        {label}
-      </Paragraph>
-    </Box>
-  )
-}
+}) => (
+  <Box
+    align={align}
+    direction="row"
+    height={height}
+    width="max-content"
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
+  >
+    {!labelOnly && (
+      <Box margin={{ right: 'xxsmall' }} height={height}>
+        <Icon
+          color={color || types[type].color}
+          name={types[type].icon}
+          size={iconSize}
+        />
+      </Box>
+    )}
+    <Paragraph color={fontColor || types[type].fontColor} size={fontSize}>
+      {label}
+    </Paragraph>
+  </Box>
+)
 
 export default InlineMessage
