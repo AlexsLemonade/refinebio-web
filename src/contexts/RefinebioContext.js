b594571c62ecadc0db06f297b83dc8fd5c334d26
Nozomi Ichihara <nozomione.online@gmail.com>
2024-01-16 16:08:26 -0500

    (edit) add a check for 'dataSetId' value (old storage key). If it exists, assign it to 'datasetId' (new storage key) and remove the old key from localStorage

--- src/contexts/RefinebioContext.js
+++ src/contexts/RefinebioContext.js
@@ -24,9 +24,15 @@ export const RefinebioContextProvider = ({ children }) => {
   // NOTE: migration support is removed 12 months after the site swap
   useEffect(() => {
     if (pageRendered) {
+      // after the code swap, if the old key exists in the users' browsers, assign that
+      // value to the new key to prevent them from losing their customized dataset
       const oldKey = 'dataSetId'
-      setDatasetId(getOldLocalStorageKey(oldKey) || datasetId)
-      removeOldLocalStorageKey(oldKey)
+      const oldKeyValue = getOldLocalStorageKey(oldKey)
+
+      if (oldKeyValue) {
+        setDatasetId(getOldLocalStorageKey(oldKeyValue))
+        removeOldLocalStorageKey(oldKey)
+      }
     }
   }, [pageRendered])
 
