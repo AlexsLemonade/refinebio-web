3ae074a7e010f80e1311e9eb0f6b14b06caf2383
Nozomi Ichihara <nozomione.online@gmail.com>
2024-08-08 10:11:35 -0400

    (re-format/edit) move the 'Slack Port' comment outside of async function, move the const var 'response' outside of try, return resonse and remove res.end

--- src/pages/api/data-requests.js
+++ src/pages/api/data-requests.js
@@ -2,10 +2,11 @@ import { submitGithubDataRequest } from 'data-requests/github'
 import { submitHubspotDataRequest } from 'data-requests/hubspot'
 import { submitSlackDataRequest } from 'data-requests/slack'
 
-export default async (req, res) => {
-  /* 
-  Examples of Skack Post Data
+/* 
+  Examples of Slack Post
+
   For 'experiment`:
+  ------------------------------------------------------------------------------------------------
   SRP152576 Experiment Requested // Hyperlinked heading (e.g., https://www.refine.bio/experiments/SRP152576)
   Pediatric Cancer Research   Primary Approach
   Yes                         Clinical Research
@@ -16,8 +17,10 @@ export default async (req, res) => {
   
   Refine.bio | User's IP address | Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:120.0) Gecko/20100101
   Firefox/120.0 | This message was sent because the request to GitHub and HubSpot failed | Jun 14th, 2024
+  ------------------------------------------------------------------------------------------------
 
   For 'search`:
+  ------------------------------------------------------------------------------------------------
   Missing data for search term 'E-MEXP-3405' // Hyperlinked heading (e.g., https://www.refine.bio/search?search=E-MEXP-3405)
   Accession Codes           Pediatric Cancer Research
   E-MEXP-3405, SRP2422      Yes
@@ -30,8 +33,10 @@ export default async (req, res) => {
 
   Refine.bio | User's IP address | Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:120.0) Gecko/20100101
   Firefox/120.0 | This message was sent because the request to GitHub failed | Jun 14th, 2024
-  */
+  ------------------------------------------------------------------------------------------------
+*/
 
+export default async (req, res) => {
   // requestValues should include form values, query/accessionCode, and request_type ('experiment' or 'search')
   const {
     body: {
@@ -47,19 +52,22 @@ export default async (req, res) => {
     return res.status(405).json({ message: `Method ${method} Not Allowed` })
   }
 
+  const response = { status: null, message: '' }
+
   try {
-    const response = { status: null }
+    // submits data to Github API
     const githubSuccess = await submitGithubDataRequest(
       process.env.GITHUB_TOKEN,
       requestValues,
       requestType
     )
+    // submits data to Hubspot API
     const hubspotSuccess = await submitHubspotDataRequest(
       process.env.HUBSPOT_ACCESS_TOKEN,
       requestValues,
       requestType
     )
-    // requests to Slack only if requests to GitHub and/or HubSpot fail
+    // requests to Slack only if GitHub and/or HubSpot submissions fail
     if (!githubSuccess || !hubspotSuccess) {
       // sets failed requests' API name(s) to print
       const bothFailed = !githubSuccess && !hubspotSuccess
@@ -86,13 +94,11 @@ export default async (req, res) => {
       response.message = 'GitHub and HubSpot succeeded'
     }
 
-    res.status(response.status).json(response)
+    return res.status(response.status).json(response)
   } catch (error) {
     console.error('Internal Server Error:', error)
-    res.status(500).json({
+    return res.status(500).json({
       message: 'Internal Server Error'
     })
   }
-
-  return res.end()
 }
