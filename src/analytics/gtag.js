89e1d8065aef75a9c523ac353a76e022e8af299e
Nozomi Ichihara <nozomione.online@gmail.com>
2024-08-16 09:10:13 -0400

    (edit) add comments for each functions to describe its purpose and behavior, and a note comment about all custom event parameter names need to registered in GA

--- src/analytics/gtag.js
+++ src/analytics/gtag.js
@@ -2,6 +2,9 @@ import { links, options } from 'config'
 import formatFacetNames from 'helpers/formatFacetNames'
 import formatFilterName from 'helpers/formatFilterName'
 
+// NOTE: User-defined event parameters must be configured as custom definitions in the GA property.
+// Please make sure they are registered if you create any new ones.
+
 const { transformation: defaultTransformation } = options
 //  converts defaultTransformation to an object with { value: label } pair for quick lookup
 // i.e., { NONE: 'None', MINMAX: 'Zero to One', STANDARD: 'Z-score' }
@@ -20,16 +23,19 @@ const event = (eventName, value = {}, nonInteraction = false) => {
 }
 
 /* --- General --- */
+// tracks email subscriptions and the user-subscribed location
 const emailSubscription = (from) =>
   event('email_subscription', { subscription_click_from: from })
 
 /* --- Compendia --- */
+// tracks the compendia downloads and organism names
 const compendiaDownload = (type, organism) =>
   event(`compendia_${type}_download`, {
     [`${type}_organism`]: organism
   })
 
 /* --- Datasets --- */
+// tracks user clicks on the dataset actions buttons
 const myDatasetAction = (action) =>
   event('my_dataset_action', {
     my_dataset_action: `${action[0].toUpperCase()}${action.substring(
@@ -37,6 +43,7 @@ const myDatasetAction = (action) =>
     )} Samples`
   })
 
+// tracks the dataset download options selected by the user
 const myDatasetDownloadOptions = (dataset) =>
   event('my_dataset_download_options', {
     my_dataset_download_options: `Aggregate: ${
@@ -47,13 +54,16 @@ const myDatasetDownloadOptions = (dataset) =>
   `
   })
 
+// tracks the number of dataset downloads associated with each token
 const datasetDownload = (token) => event('dataset_downalod', { token })
 
+// tracks the number of one-off downloads associated with each accession code
 const oneOffExperimentDownload = (accessionCode) =>
   event('one_off_experiment_download', {
     one_off_experiment_download: accessionCode
   })
 
+// tracks the dataset's state (expired or valid) and changes in download options (initial and updated)
 const regeneratedDataset = (state, defaultOptions, newOptions) => {
   // due to GA char limit, keys names are abbreviated
   const format = (option) => {
@@ -70,25 +80,30 @@ const regeneratedDataset = (state, defaultOptions, newOptions) => {
   })
 }
 
+// tracks user clicks on the share dataset button
 const sharedDataset = (isProcessed) =>
   event('shared_dataset', {
     shared_state: isProcessed ? 'Processed' : 'Unprocessed'
   })
 
 /* --- Links --- */
+// tracks click-through to the experiment page from search results
 const experimentPageClick = (from) =>
   event('page_view', {
     experiment_page_click_from: from
   })
 
+// tracks which "Explore what you can do with your dataset" link users click on
 const exploredUsageClick = (usage, type = 'dataset') =>
   event(`click`, { [`${type}_explored_usage`]: usage })
 
+// tracks the global navigation clicks
 const navClick = (item, type = 'global') =>
   event(`page_view`, {
     [`${type}_nav_item`]: item
   })
 
+// tracks the outbound link clicks
 const outboundClick = (url) => {
   // if url starts with one of below links, sets linkName to its corresponding key
   const specialCases = [
@@ -111,6 +126,7 @@ const outboundClick = (url) => {
 }
 
 /* --- Search --- */
+// tracks the most used filter combinations
 const filterCombination = (facets, query) => {
   const filters = getSortedFilters(facets, query)
   const combination = getFilterCombination(filters)
@@ -118,13 +134,16 @@ const filterCombination = (facets, query) => {
   event('page_view', { filter_combination: combination })
 }
 
+// tracks the types of filters being used the most (i.e., organism, platform, technology)
 const filterType = (type) => event('filter_type', { filter_type: type })
 
+// tracks user-interactions with toggling filters on and off
 const toggleFilterItem = (checked, item) =>
   event('toggled_filter_item', {
     toggled_filter_item: `${checked ? 'Add' : 'Remove'} - ${item}`
   })
 
+// tracks the user-entered search terms
 const searchTerm = (term) => event('search_text', { search_text: term })
 
 // helpers
