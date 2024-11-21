AlexsLemonade/refinebio-web

commit 753aef5fb9738b95fc3b0e729bb24cf9f70385bf

Merge feature/epic-refactor-dataset-page-data-handling into dev (#403)
* 360 - Refactor Dataset Page to handle dataset ID via URL query (#364)

* (edit) remove states for different dataset type, and pass the entire data structure (i.e., 'dataset') to sub-components, define dataset state variables (i.e., 'isProcessed', 'isUnprocessed') in sub-components and clean up the JSX rendering condition, in pages/dataset/[datast_id] and pages/download

* (edit) make explicit return for 'loading' in [dataset_id] instead of ternary in JSX, and destructure 'dataset' in DatasetSummary

* (rename and cleanup) use the given props as is without renaming in components, and remove the conditional check for data (no longer required) in DatasetPageHader

* (minor) use the name myDataset for user-defined dataset, and get its 'id' and 'data' (until we rename the user-defined 'dataset' context state to 'myDataset' in a separate issue)

* (edit) revert the change - this will be handled using the getDatasetState helper in a later PR

* (clean up) remove the data object check from JSX in [dataset_id]

* 359 - Handle FileSummary rendering using local state (#365)

* (edit) remove states for different dataset type, and pass the entire data structure (i.e., 'dataset') to sub-components, define dataset state variables (i.e., 'isProcessed', 'isUnprocessed') in sub-components and clean up the JSX rendering condition, in pages/dataset/[datast_id] and pages/download

* (edit) make explicit return for 'loading' in [dataset_id] instead of ternary in JSX, and destructure 'dataset' in DatasetSummary

* (fix) add a check for regeneratedDatasetto prevent TypeError undefined to getDatasetOptionsChanges in analytics/helpers

* (remove) clean up the global context state 'regeneratedDataset' from RefinebioContext, useRefinebio, and DatasetManagerContext

src/components/Download/DatasetSummary/index.js
@@ -38,7 +38,6 @@ const getExperimentCountBySpecies = (data, experiments) => {
 export const DatasetSummary = ({ dataset }) => {
   const { getTotalExperiments, getTotalSamples } = useDatasetManager()
   const { setResponsive } = useResponsive()
-  const samplesBySpecies = dataset.organism_samples
   const totalSamples = getTotalSamples(dataset.data)
   const totalExperiments = getTotalExperiments(dataset.data)
   const experimentCountBySpecies = getExperimentCountBySpecies(
@@ -71,7 +70,7 @@ export const DatasetSummary = ({ dataset }) => {
           </TableHeader>
           <TableBody style={{ fontSize: setResponsive('16px', '18px') }}>
             <SpiecesRow
-              samplesBySpecies={samplesBySpecies}
+              samplesBySpecies={dataset.organism_samples}
               experimentCountBySpecies={experimentCountBySpecies}
             />
             <TotalRow totals={[totalSamples, totalExperiments]} />

src/components/Download/DownloadOptionsForm/index.js
@@ -4,6 +4,7 @@ import { Formik } from 'formik'
 import { Box, Form } from 'grommet'
 import { useDatasetManager } from 'hooks/useDatasetManager'
 import { useResponsive } from 'hooks/useResponsive'
+import getDatasetState from 'helpers/getDatasetState'
 import { Button } from 'components/shared/Button'
 import { Row } from 'components/shared/Row'
 import { AdvancedOptions } from './AdvancedOptions'
@@ -12,33 +13,33 @@ import { AggregateOptions } from './AggregateOptions'
 import { TransformationOptions } from './TransformationOptions'
 
 export const DownloadOptionsForm = ({
-  dataset = null,
+  dataset,
   buttonLabel = 'Download',
-  isProcessed = false,
-  onSubmit = null
+  handleDownloadOptionsChanges = null, // for the regenerate dataset local state update
+  onSubmit = null // for the regenerate dataset download
 }) => {
   const { push } = useRouter()
-  const {
-    dataset: datasetState,
-    createDataset,
-    updateDataset,
-    regeneratedDataset,
-    getDownloadOptions,
-    updateDownloadOptions
-  } = useDatasetManager()
+  const { updateDataset, getDownloadOptions, updateDownloadOptions } =
+    useDatasetManager()
   const { setResponsive } = useResponsive()
-  const selectedDataset = dataset || datasetState
+  const [canRegenerate, setCanRegenerate] = useState(false)
   const [toggleAdvancedOption, setToggleAdvancedOption] = useState(
-    selectedDataset.quantile_normalize
+    dataset?.quantile_normalize
   )
 
   useEffect(() => {
-    if (selectedDataset) {
-      updateDownloadOptions({
-        ...getDownloadOptions(selectedDataset)
-      })
-    }
-  }, [])
+    if (!dataset) return
+    // sets the initial download options
+    const { isProcessed } = getDatasetState(dataset)
+    setCanRegenerate(isProcessed)
+    updateDownloadOptions(
+      {
+        ...getDownloadOptions(dataset)
+      },
+      dataset.id,
+      isProcessed
+    )
+  }, [dataset])
 
   const handleSubmitForm = async (downloadOptions) => {
     let pathname = '/download'
@@ -46,11 +47,12 @@ export const DownloadOptionsForm = ({
     if (onSubmit) {
       const response = await onSubmit(downloadOptions)
       pathname = response
-    } else {
-      const datasetToUpdate = isProcessed ? regeneratedDataset : selectedDataset
-      await updateDataset(datasetToUpdate.id, {
+    }
+    // updates via API call only for My Dataset in /download (unprocessed)
+    if (!canRegenerate) {
+      await updateDataset(dataset.id, {
         ...downloadOptions,
-        data: datasetToUpdate.data
+        data: dataset.data
       })
     }
 
@@ -65,80 +67,81 @@ export const DownloadOptionsForm = ({
     )
   }
 
-  const handleUpdateDownloadOptions = async (name, newValue) => {
-    // eslint-disable-next-line no-nested-ternary
-    const datasetId = isProcessed
-      ? regeneratedDataset
-        ? regeneratedDataset.id
-        : await createDataset()
-      : selectedDataset.id
+  const handleUpdateDownloadOptions = async (name, newOption) => {
+    const newDownloadOption = { [name]: newOption }
 
-    await updateDownloadOptions({ [name]: newValue }, datasetId, isProcessed)
+    if (canRegenerate && handleDownloadOptionsChanges) {
+      handleDownloadOptionsChanges(newDownloadOption)
+    }
+
+    await updateDownloadOptions(newDownloadOption, dataset.id, canRegenerate)
   }
 
   return (
     <Box border={{ side: 'bottom' }}>
-      <Formik
-        initialValues={{
-          aggregate_by: selectedDataset.aggregate_by,
-          data: selectedDataset.data,
-          scale_by: selectedDataset.scale_by,
-          quantile_normalize: selectedDataset.quantile_normalize
-        }}
-        onSubmit={async (values, { setSubmitting }) => {
-          await handleSubmitForm(values)
-          setSubmitting(false)
-        }}
-      >
-        {({ handleChange, handleSubmit, isSubmitting, values }) => (
-          <Form onSubmit={handleSubmit}>
-            <Row direction={setResponsive('column', 'column', 'row')}>
-              <Box>
-                <Row align={setResponsive('start', 'center')} justify="start">
-                  <AggregateOptions
-                    value={values.aggregate_by}
-                    handleChange={handleChange}
-                    handleUpdateDownloadOptions={handleUpdateDownloadOptions}
-                  />
-                  <TransformationOptions
-                    value={values.scale_by}
-                    handleChange={handleChange}
-                    handleUpdateDownloadOptions={handleUpdateDownloadOptions}
-                  />
-                  <Box
-                    margin={{
-                      top: setResponsive('xsmall', 'none'),
-                      left: setResponsive('none', 'xsmall')
-                    }}
-                  >
-                    <AdvancedOptionsToggle
-                      toggle={toggleAdvancedOption}
-                      setToggle={setToggleAdvancedOption}
+      {dataset && (
+        <Formik
+          initialValues={{
+            aggregate_by: dataset.aggregate_by,
+            data: dataset.data,
+            scale_by: dataset.scale_by,
+            quantile_normalize: dataset.quantile_normalize
+          }}
+          onSubmit={async (values, { setSubmitting }) => {
+            await handleSubmitForm(values)
+            setSubmitting(false)
+          }}
+        >
+          {({ handleChange, handleSubmit, isSubmitting, values }) => (
+            <Form onSubmit={handleSubmit}>
+              <Row direction={setResponsive('column', 'column', 'row')}>
+                <Box>
+                  <Row align={setResponsive('start', 'center')} justify="start">
+                    <AggregateOptions
+                      value={values.aggregate_by}
+                      handleChange={handleChange}
+                      handleUpdateDownloadOptions={handleUpdateDownloadOptions}
                     />
-                  </Box>
-                </Row>
-                <AdvancedOptions
-                  id={selectedDataset.id}
-                  values={values}
-                  toggle={!toggleAdvancedOption}
-                  handleChange={handleChange}
-                  handleUpdateDownloadOptions={handleUpdateDownloadOptions}
+                    <TransformationOptions
+                      value={values.scale_by}
+                      handleChange={handleChange}
+                      handleUpdateDownloadOptions={handleUpdateDownloadOptions}
+                    />
+                    <Box
+                      margin={{
+                        top: setResponsive('xsmall', 'none'),
+                        left: setResponsive('none', 'xsmall')
+                      }}
+                    >
+                      <AdvancedOptionsToggle
+                        toggle={toggleAdvancedOption}
+                        setToggle={setToggleAdvancedOption}
+                      />
+                    </Box>
+                  </Row>
+                  <AdvancedOptions
+                    id={dataset.id}
+                    values={values}
+                    toggle={!toggleAdvancedOption}
+                    handleChange={handleChange}
+                    handleUpdateDownloadOptions={handleUpdateDownloadOptions}
+                  />
+                </Box>
+                <Button
+                  label={buttonLabel}
+                  isLoading={isSubmitting}
+                  primary
+                  responsive
+                  margin={{
+                    bottom: setResponsive('small', 'small', 'none')
+                  }}
+                  type="submit"
                 />
-              </Box>
-              <Button
-                label={buttonLabel}
-                isLoading={isSubmitting}
-                primary
-                responsive
-                margin={{
-                  bottom: setResponsive('small', 'small', 'none')
-                }}
-                type="submit"
-              />
-            </Row>
-          </Form>
-        )}
-      </Formik>
+              </Row>
+            </Form>
+          )}
+        </Formik>
+      )}
     </Box>
   )
 }

src/components/Download/DatasetDetails/ExperimentView/index.js
@@ -1,71 +1,42 @@
-import { useState } from 'react'
-import { Paragraph } from 'grommet'
-import { useDatasetManager } from 'hooks/useDatasetManager'
-import getReadableOptions from 'helpers/getReadableOptions'
+import { useEffect, useState } from 'react'
 import { OrganismFilter } from './OrganismFilter'
 import { ViewBlock } from './ViewBlock'
 import { ViewBlocks } from '../ViewBlocks'
 
-export const ExperimentView = ({
-  dataset: {
-    data: datasetData,
-    id: datasetId,
-    experiments,
-    quantile_normalize: quantileNormalize
-  },
-  isImmutable
-}) => {
-  const { formatSampleMetadata } = useDatasetManager()
-  const defaultValue = 'ALL'
-  const defaultOrganismFilterOption = getReadableOptions([defaultValue])[0]
-  const [organism, setOrganism] = useState(defaultValue)
+export const ExperimentView = ({ dataset, isImmutable }) => {
+  const defaultOption = { label: 'All Speciess', value: 'ALL' }
+  const [organism, setOrganism] = useState(defaultOption.value)
+  const filteredExperiments = dataset.experiments.filter(
+    (experiment) =>
+      organism === defaultOption.value ||
+      experiment.organism_names.includes(organism)
+  )
 
-  if (!datasetData || !Object.keys(datasetData).length) {
-    return <Paragraph>No samples added to download dataset.</Paragraph>
-  }
+  // resets the organism filter to default on dataset change
+  useEffect(() => {
+    setOrganism(defaultOption.value)
+  }, [dataset])
 
   return (
     <>
-      <OrganismFilter
-        datasetData={datasetData}
-        defaultOption={defaultOrganismFilterOption}
-        experiments={experiments}
-        organism={organism}
-        setOrganism={setOrganism}
-      />
-      {experiments && (
-        <ViewBlocks elevation="medium" pad="medium">
-          {Object.keys(datasetData).map((experimentAccessionCode) => {
-            const addedSamples = datasetData[experimentAccessionCode]
-            const experiment = experiments[experimentAccessionCode]
-            const metadataFields = formatSampleMetadata(
-              experiment.sample_metadata
-            )
-
-            if (
-              organism !== defaultValue &&
-              !experiment.organism_names.includes(organism)
-            ) {
-              return null
-            }
-
-            return (
-              <ViewBlock
-                key={experimentAccessionCode}
-                addedSamples={addedSamples}
-                datasetId={datasetId}
-                defaultOrganismFilterOption={defaultOrganismFilterOption}
-                experiment={experiment}
-                experimentAccessionCode={experimentAccessionCode}
-                metadataFields={metadataFields}
-                quantileNormalize={quantileNormalize}
-                isImmutable={isImmutable}
-                setOrganism={setOrganism}
-              />
-            )
-          })}
-        </ViewBlocks>
+      {Object.keys(dataset.organism_samples).length > 1 && (
+        <OrganismFilter
+          dataset={dataset}
+          defaultOption={defaultOption}
+          organism={organism}
+          setOrganism={setOrganism}
+        />
       )}
+      <ViewBlocks elevation="medium" pad="medium">
+        {filteredExperiments.map((experiment) => (
+          <ViewBlock
+            key={experiment.accession_code}
+            dataset={dataset}
+            experiment={experiment}
+            isImmutable={isImmutable}
+          />
+        ))}
+      </ViewBlocks>
     </>
   )
 }

src/components/Download/DatasetDetails/SpeciesView/index.js
@@ -1,64 +1,18 @@
-import unionizeArrays from 'helpers/unionizeArrays'
 import { ViewBlock } from './ViewBlock'
 import { ViewBlocks } from '../ViewBlocks'
 
-export const SpeciesView = ({
-  dataset: {
-    data: datasetData,
-    id: datasetId,
-    experiments,
-    organism_samples: samplesBySpecies,
-    quantile_normalize: quantileNormalize
-  },
-  isImmutable
-}) => {
+export const SpeciesView = ({ dataset, isImmutable }) => {
+  const { organism_samples: organismSamples } = dataset
+  const organismsNames = Object.keys(organismSamples)
+
   return (
     <ViewBlocks elevation="medium" pad="medium">
-      {Object.keys(samplesBySpecies).map((specieName) => {
-        // get the accession codes accosiated with the specieName
-        const samplesInSpecie = samplesBySpecies[specieName]
-        // filter the dataset to only include the experiments containing the samplesInSpecie
-        const specieDatasetSlice = Object.entries(datasetData).reduce(
-          (accumulator, [key, value]) => {
-            accumulator[key] = value.filter((accessionCode) =>
-              samplesInSpecie.includes(accessionCode)
-            )
-            return accumulator
-          },
-          {}
-        )
-        // merge all the sample metadata fields of all experiments containing the specieDatasetSlice
-        // in order to display all possible values of these samples
-        const sampleMetadataFields = unionizeArrays(
-          ...Object.keys(specieDatasetSlice)
-            .filter(
-              (accessionCode) => specieDatasetSlice[accessionCode].length > 0
-            )
-            .map((accessionCode) => {
-              return (
-                experiments[accessionCode] &&
-                experiments[accessionCode].sample_metadata
-              )
-            })
-        )
-        // NOTE: if some of the experiments have samples of the same organism and it's also rna seq,
-        // then we can deduce there're rna seq samples for this organism
-        const hasRnaSeqExperiments = Object.values(experiments).some(
-          (experiment) =>
-            experiment.technology === 'RNA-SEQ' &&
-            experiment.organism_names.includes(specieName)
-        )
-
+      {organismsNames.map((organismName) => {
         return (
           <ViewBlock
-            key={specieName}
-            datasetId={datasetId}
-            hasRnaSeqExperiments={hasRnaSeqExperiments}
-            sampleMetadataFields={sampleMetadataFields}
-            samplesInSpecie={samplesInSpecie}
-            specieDatasetSlice={specieDatasetSlice}
-            specieName={specieName}
-            quantileNormalize={quantileNormalize}
+            key={organismName}
+            dataset={dataset}
+            organismName={organismName}
             isImmutable={isImmutable}
           />
         )

src/components/shared/DatasetActionButton/index.js
@@ -1,6 +1,5 @@
 import { useDatasetManager } from 'hooks/useDatasetManager'
 import { useDatasetAction } from 'hooks/useDatasetAction'
-import { usePageRendered } from 'hooks/usePageRendered'
 import { Button } from 'components/shared/Button'
 import { AddRemainingDatasetButton } from './AddRemainingDatasetButton'
 import { AddToDatasetButton } from './AddToDatasetButton'
@@ -14,9 +13,6 @@ export const DatasetActionButton = ({
   ...props
 }) => {
   const { dataset } = useDatasetManager()
-  const pageRendered = usePageRendered()
-
-  if (!pageRendered) return null
 
   const {
     getHasAllProcessed,

src/pages/download/index.js
@@ -72,7 +72,7 @@ export const Download = () => {
                 </Heading>
                 <ShareDatasetButton dataset={dataset} />
               </Row>
-              <DownloadOptionsForm />
+              <DownloadOptionsForm dataset={dataset} />
               <FilesSummary dataset={dataset} />
               <DatasetSummary dataset={dataset} />
               <DatasetDetails dataset={dataset} />

src/analytics/gtag.js
@@ -67,7 +67,7 @@ const trackOneOffExperimentDownload = (experiment) => {
   payload.one_off_experiment_download = experiment.accession_code
   event('one_off_experiment_download', payload)
 }
-// tracks the dataset's state (expired or valid) and changes in download options (initial and updated)
+// tracks the dataset's state (expired or valid) and download options change (initial and updated)
 const trackRegeneratedDataset = (dataset, regeneratedDataset) => {
   const payload = {}
   payload.regenerated_state = getDatasetState(dataset)

src/analytics/helpers.js
@@ -79,22 +79,25 @@ export const event = (eventName, value = {}, nonInteraction = false) => {
 }
 
 export const getDatasetOptionsChanges = (dataset, regeneratedDataset) => {
-  const changes = datasetOptionsKeys.map((key) => {
-    return `${getReadable(key, dataset[key])} -> ${getReadable(
-      key,
-      regeneratedDataset[key]
-    )}`
-  })
-  return changes.join('|')
+  const changes = datasetOptionsKeys
+    .map((key) => {
+      const initialOption = getReadable(dataset[key])
+      const regeneratedOption = getReadable(regeneratedDataset[key])
+
+      return initialOption !== regeneratedOption
+        ? `${initialOption} -> ${regeneratedOption}`
+        : null
+    })
+    .filter(Boolean)
+
+  return changes.length > 0 ? changes.join('|') : 'No change'
 }
 
 export const getDatasetState = (dataset) =>
   getReadable('expires_on', moment(dataset.expires_on).isBefore(Date.now()))
 
 export const getFormattedDatasetOptions = (dataset) =>
-  `${datasetOptionsKeys
-    .map((key) => `${getReadable(key, dataset[key])}`)
-    .join('|')}`
+  `${datasetOptionsKeys.map((key) => `${getReadable(dataset[key])}`).join('|')}`
 
 // formats filter names in each facet
 const formatFacets = (query) => {

commit 9a9962865c5b04f2a58f9760405a563fb1da0e34

Fix bugs introduced in #403 merge in dev  (#418)
* (fix) remove 'dataset' from useEffect dependency in DownloadOptionForm (accidentally overwritten during resolving conflicts), use ‘find’ to retrieve the experiment in getExperimentCountByOrganisms (formerly getExperimentCountBySpecies, renamed to reflect latest dev changes) in DatasetSummary, add ‘length’ property for if check in helpers/getPlatformNamesExperiment, ue early return (for hasSamples) in the Experiment page component to prevent hydration errors , (temp) temporarily add isPageReady in Search page component to prevent hydration error

* (edit) remove teh accession code check getExperimentCountByOrganisms and directly count the experiment per organis instead

* (edit) pass the 'dataset' prop to SpeciesRow and TotalRow (remove individual props) and move the calculation logic into them, replace getExperimentCountByOrganisms with a new generic helper getArrayCounts, (minor) rename specie to organism to match the latest dev

src/components/Download/DatasetSummary/SpeciesRow.js
@@ -1,28 +1,32 @@
 import { TableCell, TableRow, Text } from 'grommet'
+import getArrayCounts from 'helpers/getArrayCounts'
 import formatNumbers from 'helpers/formatNumbers'
 import formatString from 'helpers/formatString'
 
-export const SpiecesRow = ({
-  samplesBySpecies = [],
-  experimentCountBySpecies = []
-}) => (
-  <>
-    {Object.keys(samplesBySpecies).map((organism) => (
-      <TableRow key={organism}>
-        <TableCell>{formatString(organism)}</TableCell>
-        <TableCell>
-          <Text color="brand">
-            {formatNumbers(samplesBySpecies[organism]?.length)}
-          </Text>
-        </TableCell>
-        <TableCell>
-          <Text color="brand">
-            {formatNumbers(experimentCountBySpecies[organism]) || 0}
-          </Text>
-        </TableCell>
-      </TableRow>
-    ))}
-  </>
-)
+export const SpiecesRow = ({ dataset }) => {
+  const { experiments, organism_samples: organismSamples } = dataset
+  const experimentCountByOrganism = getArrayCounts(
+    experiments.map((e) => e.organism_names).flat()
+  )
 
+  return (
+    <>
+      {Object.keys(organismSamples).map((organism) => (
+        <TableRow key={organism}>
+          <TableCell>{formatString(organism)}</TableCell>
+          <TableCell>
+            <Text color="brand">
+              {formatNumbers(organismSamples[organism]?.length)}
+            </Text>
+          </TableCell>
+          <TableCell>
+            <Text color="brand">
+              {formatNumbers(experimentCountByOrganism[organism]) || 0}
+            </Text>
+          </TableCell>
+        </TableRow>
+      ))}
+    </>
+  )
+}
 export default SpiecesRow

src/components/Download/DatasetSummary/TotalRow.js
@@ -1,8 +1,15 @@
 import { nanoid } from 'nanoid'
 import { TableCell, TableRow, Text } from 'grommet'
+import { useDatasetManager } from 'hooks/useDatasetManager'
 import formatNumbers from 'helpers/formatNumbers'
 
-export const TotalRow = ({ totals = [] }) => {
+export const TotalRow = ({ dataset }) => {
+  const { getTotalExperiments, getTotalSamples } = useDatasetManager()
+  const totals = [
+    getTotalSamples(dataset.data),
+    getTotalExperiments(dataset.data)
+  ]
+
   return (
     <TableRow>
       <TableCell>Total</TableCell>

src/components/Download/DatasetSummary/index.js
@@ -7,43 +7,13 @@ import {
   TableHeader,
   TableRow
 } from 'grommet'
-import { useDatasetManager } from 'hooks/useDatasetManager'
 import { useResponsive } from 'hooks/useResponsive'
 import { Row } from 'components/shared/Row'
 import { SpiecesRow } from './SpeciesRow'
 import { TotalRow } from './TotalRow'
 
-// returns the count of expriment by spcecies
-const getExperimentCountBySpecies = (data, experiments) => {
-  if (!data || !experiments) return {}
-
-  const species = {}
-
-  for (const accessionCode of Object.keys(data)) {
-    const experimentInfo = experiments[accessionCode]
-
-    if (!experimentInfo) return {}
-
-    const { organism_names: organismNames } = experimentInfo
-
-    for (const organism of organismNames) {
-      if (!species[organism]) species[organism] = 0
-      species[organism] += 1
-    }
-  }
-
-  return species
-}
-
 export const DatasetSummary = ({ dataset }) => {
-  const { getTotalExperiments, getTotalSamples } = useDatasetManager()
   const { setResponsive } = useResponsive()
-  const totalSamples = getTotalSamples(dataset.data)
-  const totalExperiments = getTotalExperiments(dataset.data)
-  const experimentCountBySpecies = getExperimentCountBySpecies(
-    dataset.data,
-    dataset.experiments
-  )
 
   return (
     <Box margin={{ top: 'large' }}>
@@ -69,11 +39,8 @@ export const DatasetSummary = ({ dataset }) => {
             </TableRow>
           </TableHeader>
           <TableBody style={{ fontSize: setResponsive('16px', '18px') }}>
-            <SpiecesRow
-              samplesBySpecies={dataset.organism_samples}
-              experimentCountBySpecies={experimentCountBySpecies}
-            />
-            <TotalRow totals={[totalSamples, totalExperiments]} />
+            <SpiecesRow dataset={dataset} />
+            <TotalRow dataset={dataset} />
           </TableBody>
         </Table>
       </Row>

src/components/Download/DownloadOptionsForm/index.js
@@ -39,7 +39,7 @@ export const DownloadOptionsForm = ({
       dataset.id,
       isProcessed
     )
-  }, [dataset])
+  }, [])
 
   const handleSubmitForm = async (downloadOptions) => {
     let pathname = '/download'

src/helpers/getArrayCounts.js
@@ -1,0 +1,9 @@
+// Returns an object with the count of element occurrences in the given array
+// e.g.,
+// Input: [ 'a', 'b', 'b' ]
+// Output: { a: 1, b: 2 }
+export default (arr) =>
+  arr.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1
+    return acc
+  }, {})

src/helpers/getPlatformNamesFromExperiment.js
@@ -6,7 +6,7 @@ export default ({
   pretty_platforms: prettyPlatforms = [],
   platform_names: platformNames = []
 }) => {
-  if (prettyPlatforms) return prettyPlatforms
+  if (prettyPlatforms.length) return prettyPlatforms
 
   if (samples.length) {
     return unionizeArrays(...samples.map((sample) => sample.pretty_platform))

src/pages/experiments/[accession_code]/[title]/index.js
@@ -55,6 +55,8 @@ export const Experiment = ({ experiment }) => {
     }
   }, [fromViewSamples, hasSamples])
 
+  if (!hasSamples) return <Spinner />
+
   return (
     <>
       <PageTitle title={`${`${accessionCode} - ${experiment.title}`} -`} />
@@ -78,11 +80,7 @@ export const Experiment = ({ experiment }) => {
                 pad={setResponsive('medium', 'large')}
                 margin={{ bottom: 'basex6' }}
               >
-                {hasSamples ? (
-                  <ExperimentSamplesTable experiment={experiment} />
-                ) : (
-                  <Spinner />
-                )}
+                <ExperimentSamplesTable experiment={experiment} />
               </Box>
             </FixedContainer>
           </Box>

src/pages/search/index.js
@@ -20,6 +20,7 @@ import { Pagination } from 'components/shared/Pagination'
 import { SearchBox } from 'components/shared/SearchBox'
 import { SearchInfoBanner } from 'components/SearchResults/SearchInfoBanner'
 import { SearchCard } from 'components/shared/SearchCard'
+import { Spinner } from 'components/shared/Spinner'
 import {
   RequestSearchFormAlert,
   NoSearchResults,
@@ -56,6 +57,9 @@ export const Search = ({
   const [sortBy, setSortBy] = useState(query.sortby || sortby[0].value)
   const isResults = results?.length > 0
 
+  // TODO: Remove when refactring search in a future issue (prevent hydration error)
+  const [isPageReady, setIsPageReady] = useState(false)
+
   const handleClearSearchTerm = () => {
     setUserSearchTerm('')
   }
@@ -64,6 +68,9 @@ export const Search = ({
     e.preventDefault()
     updateSearchTerm(userSearchTerm)
   }
+  useEffect(() => {
+    setIsPageReady(true)
+  }, [])
 
   useEffect(() => {
     if (facets) {
@@ -79,11 +86,14 @@ export const Search = ({
         })
       }
     }
-  }, [])
+  }, [facets, query])
 
   useEffect(() => {
     gtag.trackSearchQuery(query)
   }, [query])
+
+  // TODO: Remove when refactring search in a future issue
+  if (!isPageReady) return <Spinner />
 
   return (
     <>