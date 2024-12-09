fad0e1f34ddedeb96716716afbe7112af112f161
Nozomi Ichihara <nozomione.online@gmail.com>
2024-12-09 15:20:00 +0000

    (edit) delete the SpeciesRow sub-component and move its logic to the DatasetSummary parent component, and use useMemo for optimization (#422)

--- src/components/Download/DatasetSummary/index.js
+++ src/components/Download/DatasetSummary/index.js
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import {
   Box,
   Heading,
@@ -5,15 +6,23 @@ import {
   TableBody,
   TableCell,
   TableHeader,
-  TableRow
+  TableRow,
+  Text
 } from 'grommet'
 import { useResponsive } from 'hooks/useResponsive'
+import getArrayCounts from 'helpers/getArrayCounts'
+import formatNumbers from 'helpers/formatNumbers'
+import formatString from 'helpers/formatString'
 import { Row } from 'components/shared/Row'
-import { SpiecesRow } from './SpeciesRow'
 import { TotalRow } from './TotalRow'
 
 export const DatasetSummary = ({ dataset }) => {
   const { setResponsive } = useResponsive()
+  const { experiments, organism_samples: organismSamples } = dataset
+  const experimentCountByOrganism = useMemo(
+    () => getArrayCounts(experiments.map((e) => e.organism_names).flat()),
+    [experiments]
+  )
 
   return (
     <Box margin={{ top: 'large' }}>
@@ -39,7 +48,21 @@ export const DatasetSummary = ({ dataset }) => {
             </TableRow>
           </TableHeader>
           <TableBody style={{ fontSize: setResponsive('16px', '18px') }}>
-            <SpiecesRow dataset={dataset} />
+            {Object.keys(organismSamples).map((organism) => (
+              <TableRow key={organism}>
+                <TableCell>{formatString(organism)}</TableCell>
+                <TableCell>
+                  <Text color="brand">
+                    {formatNumbers(organismSamples[organism].length)}
+                  </Text>
+                </TableCell>
+                <TableCell>
+                  <Text color="brand">
+                    {formatNumbers(experimentCountByOrganism[organism])}
+                  </Text>
+                </TableCell>
+              </TableRow>
+            ))}
             <TotalRow dataset={dataset} />
           </TableBody>
         </Table>
