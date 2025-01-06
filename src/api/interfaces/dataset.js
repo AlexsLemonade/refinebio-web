caa9d24f727b765a5d4d600e4043896fdb2cf925
Nozomi Ichihara <nozomione.online@gmail.com>
2025-01-06 12:16:22 -0500

    (edit) add the trailing slash using template strings and rename 'url' to 'endpoint' for clarity, (minor) remove obsolete interfaces from the api folder

--- src/api/interfaces/dataset.js
+++ src/api/interfaces/dataset.js
@@ -1,14 +1,15 @@
 import http from 'helpers/http'
 
-const url = 'dataset/'
+const endpoint = 'dataset'
 
 // By detault, considering the futhre backend API revision, pass the query string details=true (no longer optional) for fetching the dataset
 // (ref) https://github.com/AlexsLemonade/refinebio-web/issues/178#issuecomment-1719684364
 // 'details': https://github.com/AlexsLemonade/refinebio-frontend/pull/485
 
 export default {
-  create: (body) => http.post(url, body),
-  get: (id, headers) => http.get(`${url}${id}/`, { details: true }, headers),
+  create: (body) => http.post(`${endpoint}/`, body),
+  get: (id, headers) =>
+    http.get(`${endpoint}/${id}/`, { details: true }, headers),
   update: (id, body, headers) =>
-    http.put(`${url}${id}/?details=true`, body, headers)
+    http.put(`${endpoint}/${id}/?details=true`, body, headers)
 }
