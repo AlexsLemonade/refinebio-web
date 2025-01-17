430c95beacd901d37e9fac35830e9db65a8d254a
Nozomi Ichihara <nozomione.online@gmail.com>
2025-01-17 16:18:00 -0500

    (edit) pass the the 'ref' query directory with from SearchCard's header and footer (decouple the logic from useSearchManager especially we renamed from 'search' to 'searchParams'), adjust gtag.trackLink and pages/Experiments/[accesionCode]/[title]

--- src/components/shared/SearchCard/SearchCardFooter.js
+++ src/components/shared/SearchCard/SearchCardFooter.js
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router'
 import { Box } from 'grommet'
-import { useSearchManager } from 'hooks/useSearchManager'
 import formatURLString from 'helpers/formatURLString'
 import gtag from 'analytics/gtag'
 import { Button } from 'components/shared/Button'
@@ -8,12 +7,13 @@ import { Button } from 'components/shared/Button'
 export const SearchCardFooter = ({ experiment }) => {
   const { accession_code: accessionCode, title } = experiment
   const { push } = useRouter()
-  const { setSearchParams } = useSearchManager()
 
   const handleClick = () => {
-    setSearchParams((prev) => ({ ...prev, ref: 'search' }))
     gtag.trackExperimentPageClick(SearchCardFooter)
-    push(`/experiments/${accessionCode}/${formatURLString(title)}`)
+    push({
+      pathname: `/experiments/${accessionCode}/${formatURLString(title)}`,
+      query: { ref: 'view-samples' }
+    })
   }
 
   return (
