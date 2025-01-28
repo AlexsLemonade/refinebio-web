a9cccfcb68e868a2ec37952846f2f9cb749036ae
Nozomi Ichihara <nozomione.online@gmail.com>
2025-01-28 16:49:41 +0000

    386 - Refactor MoveToDatasetButton / MoveToDatasetModal components (#435)
    * (edit) add a new 'acceptedTerms' context state and user token validation in RefinebioContext (during the initial load of the application - replacing the useToken hook) ,and adjusted the other hooks that utilize token
    
    * (edit) adjust the components (download forms) that utilize the token
    
    * (edit) add 404 check in validateToken, and add a new method activateToken for clarity
    
    * (edit) add an application token (generate the user token on inital user viist without activation if it doesn't exist) and use the side effect to sync the token state with acceptedTerms in RefinebioContext, remove the token generation logic from consumer hooks, rename the prop name 'params' to 'body' in interface s methods
    
    * (edit) adjust consumer components (for form submission and the terms of use checkbox visibility)
    
    * (edit) use accetedTerms and its setter in consumer components (remove the usage of token/applyAcceptedTerms), and use the token state in hooks instesd (added a new hook useTriggerSubmit - decuple the useEffect until adding conetxt/hook for file download and download options etc)
    
    * (edit) replace useTriggerSubmit with useWaitFor and adjust components accordingly, use this new hook in RefeinbioContext provider and add waitForToken method, use waitForToken in useDatasetManager
    
    * (edit) add the trailing slash using template strings and rename 'url' to 'endpoint' for clarity, (minor) remove obsolete interfaces from the api folder
    
    * (edit) use an early return to handle the case of no samples in myDataset in MoveToDatasetButton, rename state/variables for clarity, clean up props, optimize the action methods for the MoveToDatasetModal
    
    * (minor) rename 'handleRedirect' to 'getRedirect' in MoveToDatasetButton
    
    * (edit) update the 'MoveToDatasetModal' props and and add the form tag to handle event handlers for reset and submit, add the map object for handler methods, and move the action state from 'MoveToDatasetModal' to 'MoveToDatasetButton', and rename getRedirect to downloadRedirectWithPrefix

--- src/components/Dataset/MoveToDatasetModal.js
+++ src/components/Dataset/MoveToDatasetModal.js
@@ -1,6 +1,5 @@
-import { useRouter } from 'next/router'
-import { Box, Heading } from 'grommet'
-import gtag from 'analytics/gtag'
+import { Box, Form, Heading } from 'grommet'
+
 import { useDatasetManager } from 'hooks/useDatasetManager'
 import { useResponsive } from 'hooks/useResponsive'
 import formatNumbers from 'helpers/formatNumbers'
@@ -10,94 +9,54 @@ import { Icon } from 'components/shared/Icon'
 import { RadioButtonGroup } from 'components/shared/RadioButtonGroup'
 import { Row } from 'components/shared/Row'
 
-export const MoveToDatasetModal = ({
-  id,
-  closeModal,
-  defaultValue,
-  dataset,
-  pathname,
-  radioOptions,
-  value,
-  setValue
-}) => {
-  const { myDataset, loading, addSamples, getTotalSamples, replaceSamples } =
-    useDatasetManager()
+export const MoveToDatasetModal = ({ value, onChange, onReset, onSubmit }) => {
   const { setResponsive } = useResponsive()
-  const { push } = useRouter()
-  const totalSamples = formatNumbers(getTotalSamples(myDataset.data))
-  const newDatasetTotalSamples = formatNumbers(getTotalSamples(dataset.data))
+  const { myDataset, loading, getTotalSamples } = useDatasetManager()
 
-  const handleMoveSamples = async (action = 'append') => {
-    if (action === 'append') {
-      await addSamples(dataset.data)
-      push(
-        {
-          pathname,
-          query: {
-            message: `Appended ${newDatasetTotalSamples} samples to My Dataset`,
-            status: 'success'
-          }
-        },
-        pathname
-      )
-    } else {
-      await replaceSamples(dataset.data)
-      push(
-        {
-          pathname,
-          query: {
-            message: `Moved  ${newDatasetTotalSamples} samples to My Dataset`,
-            status: 'success'
-          }
-        },
-        pathname
-      )
-    }
-    gtag.trackDatasetAction(MoveToDatasetModal)
-    closeModal(id)
-  }
-
-  const handleClose = () => {
-    setValue(defaultValue)
-    closeModal(id)
-  }
+  const myDatasetTotalSamples = formatNumbers(getTotalSamples(myDataset.data))
+  const radioOptions = [
+    { label: 'Append samples to My Dataset', value: 'append' },
+    { label: 'Replace samples in My Dataset', value: 'replace' }
+  ]
 
   return (
-    <Box
-      align="center"
-      margin={{ bottom: 'medium' }}
-      pad={{ bottom: 'small', horizontal: 'large' }}
-      width={setResponsive('100%', '100%', '500px')}
-    >
-      <Box direction="row" gap="xsmall" margin={{ bottom: 'medium' }}>
-        <Icon color="error" name="Warning" size="medium" />
-        <Heading level={2} size="small">
-          There are {totalSamples} samples in{' '}
-          <Anchor href="/download" label="My Dataset" target="_blank" />
-        </Heading>
-      </Box>
+    <Form onReset={onReset} onSubmit={onSubmit}>
       <Box
-        margin={{ bottom: 'large' }}
-        width={{ max: setResponsive('100%', '100%', '260px') }}
+        align="center"
+        margin={{ bottom: 'medium' }}
+        pad={{ bottom: 'small', horizontal: 'large' }}
+        width={setResponsive('100%', '100%', '500px')}
       >
-        <RadioButtonGroup
-          options={radioOptions}
-          name="move-to-dataset"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
+        <Box direction="row" gap="xsmall" margin={{ bottom: 'medium' }}>
+          <Icon color="error" name="Warning" size="medium" />
+          <Heading level={2} size="small">
+            There are {myDatasetTotalSamples} samples in{' '}
+            <Anchor href="/download" label="My Dataset" target="_blank" />
+          </Heading>
+        </Box>
+        <Box
+          margin={{ bottom: 'large' }}
+          width={{ max: setResponsive('100%', '100%', '260px') }}
+        >
+          <RadioButtonGroup
+            options={radioOptions}
+            name="move-to-dataset"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+          />
+        </Box>
+        <Row justify="center" gap="small" width="100%">
+          <Button type="reset" label="Cancel" secondary responsive />
+          <Button
+            type="submit"
+            label="Move Samples"
+            isLoading={loading}
+            primary
+            responsive
+          />
+        </Row>
       </Box>
-      <Row justify="center" gap="small" width="100%">
-        <Button label="Cancel" secondary responsive onClick={handleClose} />
-        <Button
-          label="Move Samples"
-          isLoading={loading}
-          primary
-          responsive
-          onClick={() => handleMoveSamples(value)}
-        />
-      </Row>
-    </Box>
+    </Form>
   )
 }
 
