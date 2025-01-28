489acf9d42dcc63817255817c72a72405bcef5d0
Nozomi Ichihara <nozomione.online@gmail.com>
2025-01-28 13:39:45 -0500

    (edit) update the middleware to run only on specified paths

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,8 @@
-// For legacy URLs support
+// Legacy URLs support for compendia routes
 import { NextResponse } from 'next/server'
+
+const matcherTab = '/compendia'
+const matcherDownload = '/compendia/download'
 
 export function middleware(request) {
   const url = request.nextUrl
@@ -9,16 +12,23 @@ export function middleware(request) {
     'rna-seq-sample': '/compendia/rna-seq'
   }
 
-  if (url.pathname.startsWith('/compendia')) {
-    if (url.pathname === '/compendia/download') {
-      return NextResponse.redirect(new URL(defaultTab, url.origin))
-    }
-
+  if (url.pathname.startsWith(matcherTab)) {
+    // Redirect to the coressponding tab based on query parameter 'c'
     const c = url.searchParams.get('c')
     if (c && redirectTabs[c]) {
       return NextResponse.redirect(new URL(redirectTabs[c], url.origin))
     }
   }
 
+  // Redirect from compendia/download path to the defualt tab
+  if (url.pathname === matcherDownload) {
+    return NextResponse.redirect(new URL(defaultTab, url.origin))
+  }
+
   return NextResponse.next()
+}
+
+export const config = {
+  // Run middleware only on listed paths
+  matcher: ['/compendia', '/compendia/download']
 }
