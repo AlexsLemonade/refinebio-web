c5f69b02eecd03df1c4059506599bf73022e4dc4
Nozomi Ichihara <nozomione.online@gmail.com>
2025-01-29 15:24:58 -0500

    (edit) remove the additional constants, do strict check for each pathname, and update comments

--- src/middleware.js
+++ src/middleware.js
@@ -1,30 +1,29 @@
 // Legacy URLs support for compendia routes
 import { NextResponse } from 'next/server'
 
-const matcherTab = '/compendia'
-const matcherDownload = '/compendia/download'
-
 export function middleware(request) {
   const url = request.nextUrl
-  const defaultTab = '/compendia/normalized'
   const redirectTabs = {
-    normalized: defaultTab,
+    normalized: '/compendia/normalized',
     'rna-seq-sample': '/compendia/rna-seq'
   }
 
-  if (url.pathname.startsWith(matcherTab)) {
-    // Redirect to the coressponding tab based on query parameter 'c'
+  // Old compendia page took a query parameter 'c'
+  // This has been replaced with a path parameter.
+  if (url.pathname === '/compendia') {
     const c = url.searchParams.get('c')
     if (c && redirectTabs[c]) {
       return NextResponse.redirect(new URL(redirectTabs[c], url.origin))
     }
   }
 
-  // Redirect from compendia/download path to the defualt tab
-  if (url.pathname === matcherDownload) {
-    return NextResponse.redirect(new URL(defaultTab, url.origin))
+  // Old compendia download page took query parameters, 'organism_name' and 'url'
+  // This has been moved and nested under path parameters for the compendium type and organism name.
+  if (url.pathname === '/compendia/download') {
+    return NextResponse.redirect(new URL(redirectTabs.normalized, url.origin))
   }
 
+  // No redirect found, continue without redirecting
   return NextResponse.next()
 }
 
