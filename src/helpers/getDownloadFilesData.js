a974258faf16c10a56b68ad69609a3fc7b4b5148
Nozomi Ichihara <nozomione.online@gmail.com>
2025-05-20 12:50:19 -0400

    (edit) fix the download files summary UI to reflect the actual content of the downloaded zip files

--- src/helpers/getDownloadFilesData.js
+++ src/helpers/getDownloadFilesData.js
@@ -1,12 +1,12 @@
 import getReadable from 'helpers/getReadable'
-// metadata info of a download https://github.com/AlexsLemonade/refinebio-frontend/issues/25#issuecomment-395870627
-// Returns the file size estimates of the given dataset and its aggregate_by value (either 'EXPERIMENT' or 'SPECIES')
+// Docs: https://docs.refine.bio/en/latest/main_text.html#downloadable-files
+// The UI reflects the contents of the downloaded zip files as listed in the doc.
 export default (dataset) => {
   const aggregateBy = getReadable(dataset.aggregate_by)
-  const isExperiment = aggregateBy === 'Experiment'
   const totalExperiments = Object.keys(dataset.data).length
   const totalSpecies = Object.keys(dataset.organism_samples).length
-  const totalCount = isExperiment ? totalExperiments : totalSpecies
+  const totalCount =
+    aggregateBy === 'Experiment' ? totalExperiments : totalSpecies
 
   return {
     files: [
@@ -16,15 +16,13 @@ export default (dataset) => {
         format: 'tsv'
       },
       {
-        title: `${totalExperiments} Sample Metadata Files`,
-        description: '1 file per Experiment',
+        title: `${totalCount} Sample Metadata Files`,
+        description: `1 file per ${aggregateBy}`,
         format: 'tsv'
       },
       {
-        title: `${totalCount} ${aggregateBy} Metadata ${
-          isExperiment ? 'Files' : ''
-        }`,
-        description: `1 file per ${aggregateBy}`,
+        title: '1 Aggregate Metadata File',
+        description: '1 file per download',
         format: 'json'
       }
     ]
